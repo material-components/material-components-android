0240f57b2cda4f95e351287f01e3c30ca12ebb95
Chris Banes <chrisbanes@google.com>
2016-11-30 05:03:22 +0000

    Merge \"Fix FloatingActionButtonTest build\" into nyc-support-24.1-dev am: 102798cbda
    Change-Id: I6db667ddcbb604a1a9f19327e249d6e015473fba
    
    GitOrigin-RevId=42ccf61b5cc2f01938ac7a37b44fb51825d3c44d
    
    PiperOrigin-RevId: 140564086

--- tests/src/android/support/design/widget/FloatingActionButtonTest.java
+++ tests/src/android/support/design/widget/FloatingActionButtonTest.java
@@ -31,6 +31,8 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
+import static org.hamcrest.Matchers.not;
+
 import android.graphics.Color;
 import android.support.design.test.R;
 import android.support.design.testutils.TestUtils;
