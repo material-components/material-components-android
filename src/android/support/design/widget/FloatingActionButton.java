c4df863eec57aa465a59e2401ee2d32117c1ca41
Chris Banes <chrisbanes@google.com>
2016-11-30 01:25:03 +0000

    am e37e921e: am ea6ff8cc: am 82ba6aaa: am 5092226e: am 191d8450: resolved conflicts for merge of 7a13c848 to mnc-dev
    * commit 'e37e921e0c9035a4b1fb0d019dc19b2ea0b03c00':
      Return the correct tint/mode from FloatingActionButton
    
    GitOrigin-RevId=03c1bfb5f070231e3257cc2f16974fec94fce4f9
    
    PiperOrigin-RevId: 140549365

--- src/android/support/design/widget/FloatingActionButton.java
+++ src/android/support/design/widget/FloatingActionButton.java
@@ -25,6 +25,7 @@ import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.annotation.ColorInt;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.R;
 import android.support.v4.view.ViewCompat;
@@ -189,7 +190,10 @@ public class FloatingActionButton extends ImageView {
      * @param tint the tint to apply, may be {@code null} to clear tint
      */
     public void setBackgroundTintList(@Nullable ColorStateList tint) {
-        mImpl.setBackgroundTintList(tint);
+        if (mBackgroundTint != tint) {
+            mBackgroundTint = tint;
+            mImpl.setBackgroundTintList(tint);
+        }
     }
 
 
@@ -216,11 +220,14 @@ public class FloatingActionButton extends ImageView {
      *                 {@code null} to clear tint
      */
     public void setBackgroundTintMode(@Nullable PorterDuff.Mode tintMode) {
-        mImpl.setBackgroundTintMode(tintMode);
+        if (mBackgroundTintMode != tintMode) {
+            mBackgroundTintMode = tintMode;
+            mImpl.setBackgroundTintMode(tintMode);
+        }
     }
 
     @Override
-    public void setBackgroundDrawable(Drawable background) {
+    public void setBackgroundDrawable(@NonNull Drawable background) {
         if (mImpl != null) {
             mImpl.setBackgroundDrawable(
                     background, mBackgroundTint, mBackgroundTintMode, mRippleColor, mBorderWidth);
