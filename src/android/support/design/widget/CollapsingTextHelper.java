92056e1ee250ba909eb5eb805572b4d182b0a428
Chris Banes <chrisbanes@google.com>
2016-11-30 01:44:55 +0000

    Merge "Fix crash in CTL on pre-v18 devices with no title" into mnc-ub-dev am: df83d90f2a am: d8599b955d
    * commit 'd8599b955de3cbb843b8a8080e35a577174164e0':
      Fix crash in CTL on pre-v18 devices with no title
    
    GitOrigin-RevId=a48c9861216d92fc353507fad974de1e5dfcf762
    
    PiperOrigin-RevId: 140551343

--- src/android/support/design/widget/CollapsingTextHelper.java
+++ src/android/support/design/widget/CollapsingTextHelper.java
@@ -560,7 +560,7 @@ final class CollapsingTextHelper {
         final int w = Math.round(mTextPaint.measureText(mTextToDraw, 0, mTextToDraw.length()));
         final int h = Math.round(mTextureDescent - mTextureAscent);
 
-        if (w <= 0 && h <= 0) {
+        if (w <= 0 || h <= 0) {
             return; // If the width or height are 0, return
         }
 
