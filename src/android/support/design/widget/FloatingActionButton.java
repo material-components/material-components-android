8b483907378ec66c0f5c4ab471c67e03e3368217
Chris Banes <chrisbanes@google.com>
2016-11-30 00:53:23 +0000

    am de373c43: am 1ef42f18: am 29611577: Merge "FAB: Fix NPE with XML attribute, set clickable by default"
    * commit 'de373c43e108bc24b46c947c7a4da26774cdb874':
      FAB: Fix NPE with XML attribute, set clickable by default
    
    GitOrigin-RevId=1f054febf6dc91e7c8d9f61500e498af8582f8b4
    
    PiperOrigin-RevId: 140545843

--- src/android/support/design/widget/FloatingActionButton.java
+++ src/android/support/design/widget/FloatingActionButton.java
@@ -122,6 +122,8 @@ public class FloatingActionButton extends ImageView {
                 mBackgroundTintMode, mRippleColor);
         mImpl.setElevation(elevation);
         mImpl.setPressedTranslationZ(pressedTranslationZ);
+
+        setClickable(true);
     }
 
     @Override
@@ -206,7 +208,10 @@ public class FloatingActionButton extends ImageView {
 
     @Override
     public void setBackgroundDrawable(Drawable background) {
-        mImpl.setBackgroundDrawable(background, mBackgroundTint, mBackgroundTintMode, mRippleColor);
+        if (mImpl != null) {
+            mImpl.setBackgroundDrawable(
+                background, mBackgroundTint, mBackgroundTintMode, mRippleColor);
+        }
     }
 
     final int getSizeDimension() {
