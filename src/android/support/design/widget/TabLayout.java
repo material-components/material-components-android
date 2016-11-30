3b8cdc278da686e54a3452c7ac733cbb3d17aa7b
Chris Banes <chrisbanes@google.com>
2016-11-30 01:15:27 +0000

    am 47d8fc2d: am e85c8ed1: am 5023ec97: Merge "Revert "Fix tab blinking while fling"" into lmp-mr1-ub-dev
    * commit '47d8fc2d5c7ec7942fa2cfc58c1f6c2a4a0b1d0a':
      Revert "Fix tab blinking while fling"
    
    GitOrigin-RevId=aa10725a2aea2db10b009e62a32c2922d4260ab2
    
    PiperOrigin-RevId: 140548366

--- src/android/support/design/widget/TabLayout.java
+++ src/android/support/design/widget/TabLayout.java
@@ -337,9 +337,7 @@ public class TabLayout extends HorizontalScrollView {
 
             @Override
             public void onPageSelected(int position) {
-                if (mScrollState == ViewPager.SCROLL_STATE_IDLE) {
-                    getTabAt(position).select();
-                }
+                getTabAt(position).select();
             }
         };
     }
