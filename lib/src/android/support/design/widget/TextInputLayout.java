418555354ddac085d8d5451ffb185a48014bac22
Juan Pereira <juanmanuel.pereira1@gmail.com>
2017-01-09 23:20:13 -0300

    [TextInputLayout] Change getTypeFace annotation to @Nullable

--- lib/src/android/support/design/widget/TextInputLayout.java
+++ lib/src/android/support/design/widget/TextInputLayout.java
@@ -304,7 +304,7 @@ public class TextInputLayout extends LinearLayout {
   /**
    * Returns the typeface used for the hint and any label views (such as counter and error views).
    */
-  @NonNull
+  @Nullable
   public Typeface getTypeface() {
     return mTypeface;
   }
