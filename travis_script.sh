93dad3106fe189a45cf1682ada4794de45edab35
Miguel Yáñez Barreto <miguely@google.com>
2017-09-22 11:20:29 -0400

    move travis_wait to travis.yml

--- travis_script.sh
+++ travis_script.sh
@@ -13,12 +13,12 @@ elif [ "$TEST_TYPE" == "instrumentation" ]; then
   echo "Assembling instrumentation tests..."
   ./gradlew assemble -PdisablePreDex;
   echo "Waiting for emulator setup..."
-  travis_wait android-wait-for-emulator
+  android-wait-for-emulator
   adb shell input keyevent 82 &
   # Avoid having it lock itself again.
   adb shell svc power stayon true
   echo "Actually running instrumentation tests..."
-  travis_wait ./gradlew connectedCheck -PdisablePreDex;
+  ./gradlew connectedCheck -PdisablePreDex;
 else
   echo "Unknown test type"
   exit 1
