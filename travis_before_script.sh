7e9f1e4db05470e1ca94919d175333367cbc0f3b
Miguel Yáñez Barreto <miguely@google.com>
2017-09-22 10:30:18 -0400

    add debug to travis scripts

--- travis_before_script.sh
+++ travis_before_script.sh
@@ -4,9 +4,11 @@ set -e
 
 # Start emulator in the background if necessary.
 
-if [ "TEST_TYPE" == "instrumentation" ] ; then
-    echo "Starting AVD for API $EMULATOR_API"
-    ./travis_create_adb.sh &
+if [ "$TEST_TYPE" == "instrumentation" ] ; then
+  echo "Starting AVD for API $EMULATOR_API"
+  ./travis_create_adb.sh &
+else
+  echo "Skipping AVD for non-instrumentation build"
 fi
 
 exit 0
--- travis_script.sh
+++ travis_script.sh
@@ -3,15 +3,19 @@
 set -e
 
 if [ "$TEST_TYPE" == "unit" ]; then
-    ./gradlew assemble lint build test -PdisablePreDex;
+  echo "Starting unit tests..."
+  ./gradlew assemble lint build test -PdisablePreDex;
 elif [ "$TEST_TYPE" == "instrumentation" ]; then
-    ./gradlew assemble -PdisablePreDex;
-    travis_wait android-wait-for-emulator
-    adb shell input keyevent 82 &
-    # Avoid having it lock itself again.
-    adb shell svc power stayon true
-    travis_wait ./gradlew connectedCheck -PdisablePreDex;
+  echo "Assembling instrumentation tests..."
+  ./gradlew assemble -PdisablePreDex;
+  echo "Waiting for emulator setup..."
+  travis_wait android-wait-for-emulator
+  adb shell input keyevent 82 &
+  # Avoid having it lock itself again.
+  adb shell svc power stayon true
+  echo "Actually running instrumentation tests..."
+  travis_wait ./gradlew connectedCheck -PdisablePreDex;
 else
-    echo "Unknown test type"
-    exit 1
+  echo "Unknown test type"
+  exit 1
 fi
