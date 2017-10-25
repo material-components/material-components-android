8fcb7613fffbd133c45ab3c162b7501c47cee9c7
markwei <markwei@google.com>
2017-10-25 02:41:46 -0400

    Split Chip into two logical sub-views with an accessability delegate.
    The main chip and close icon are virtual views denoted by HOST_ID and CLOSE_ICON_VIRTUAL_ID respectively.
    They are each a node in the virtual view hierarchy, which a user can navigate through with TalkBack.
    
    The spoken prompts for focusing the virtual views are:
    Chip, checkable: "[Unchecked/checked] Chip. $chipText. Double tap to toggle."
    Chip, uncheckable: "Chip. $chipText. Double tap to activate."
    Close icon: "Close icon. Double tap to activate."
    
    The spoke prompts for actions are:
    Chip, checkable: "[Checked/not checked]"
    Chip, uncheckable: ""
    Close icon: ""
    
    PiperOrigin-RevId: 173360466

--- lib/src/android/support/design/chip/Chip.java
+++ lib/src/android/support/design/chip/Chip.java
@@ -18,24 +18,42 @@ package android.support.design.chip;
 
 import android.content.Context;
 import android.graphics.Outline;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+import android.support.annotation.CallSuper;
 import android.support.annotation.Nullable;
 import android.support.design.theme.ThemeUtils;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat.AccessibilityActionCompat;
 import android.support.v4.widget.CompoundButtonCompat;
+import android.support.v4.widget.ExploreByTouchHelper;
 import android.support.v7.widget.AppCompatCheckBox;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.SoundEffectConstants;
 import android.view.View;
 import android.view.ViewOutlineProvider;
+import android.view.accessibility.AccessibilityEvent;
 import android.widget.CompoundButton;
+import java.util.List;
 
 /**
  * Chips are compact elements that represent an attribute, text, entity, or action. They allow users
  * to enter information, select a choice, filter content, or trigger an action.
  *
  * <p>The Chip widget is a thin view wrapper around the {@link ChipDrawable}, which contains all of
- * the layout and draw logic.
+ * the layout and draw logic. The extra logic exists to support touch, mouse, keyboard, and
+ * accessibility navigation. The main chip and close icon are considered to be separate logical
+ * sub-views, and contain their own navigation behavior and state.
+ *
+ * <p>You can register a listener on the main chip with {@link #setOnClickListener(OnClickListener)}
+ * or {@link #setOnCheckedChangeListener(OnCheckedChangeListener)}. You can register a listener on
+ * the close icon with {@link #setOnCloseIconClickListener(OnClickListener)}.
  *
  * <p>Do not use the {@code android:button} attribute. It will be ignored because Chip manages its
  * own button Drawable.
@@ -44,8 +62,16 @@ import android.widget.CompoundButton;
  */
 public class Chip extends AppCompatCheckBox {
 
+  private static final int CLOSE_ICON_VIRTUAL_ID = 0;
+
   @Nullable private ChipDrawable chipDrawable;
+
+  @Nullable private OnClickListener onCloseIconClickListener;
   private boolean deferredCheckedValue;
+
+  private final ChipTouchHelper touchHelper;
+  private final Rect rect = new Rect();
+  private final RectF rectF = new RectF();
 
   public Chip(Context context) {
     this(context, null);
@@ -64,8 +90,11 @@ public class Chip extends AppCompatCheckBox {
         ChipDrawable.createFromAttributes(context, attrs, defStyleAttr, R.style.Widget_Design_Chip);
     setButtonDrawable(drawable);
 
-    initOutlineProvider();
+    touchHelper = new ChipTouchHelper(this);
+    ViewCompat.setAccessibilityDelegate(this, touchHelper);
+    ViewCompat.setImportantForAccessibility(this, ViewCompat.IMPORTANT_FOR_ACCESSIBILITY_YES);
 
+    initOutlineProvider();
     setChecked(deferredCheckedValue);
   }
 
@@ -105,6 +134,118 @@ public class Chip extends AppCompatCheckBox {
       deferredCheckedValue = checked;
     } else if (chipDrawable.isCheckable()) {
       super.setChecked(checked);
+    }
+  }
+
+  /** Register a callback to be invoked when the close icon is clicked. */
+  public void setOnCloseIconClickListener(OnClickListener listener) {
+    this.onCloseIconClickListener = listener;
+  }
+
+  /**
+   * Call this chip's {@link #onCloseIconClickListener}, if it is defined. Performs all normal
+   * actions associated with clicking: reporting accessibility event, playing a sound, etc.
+   *
+   * @return True there was an assigned {@link #onCloseIconClickListener} that was called, false
+   *     otherwise is returned.
+   */
+  @CallSuper
+  public boolean performCloseIconClick() {
+    playSoundEffect(SoundEffectConstants.CLICK);
+
+    boolean result;
+    if (onCloseIconClickListener != null) {
+      onCloseIconClickListener.onClick(this);
+      result = true;
+    } else {
+      result = false;
+    }
+
+    touchHelper.sendEventForVirtualView(
+        CLOSE_ICON_VIRTUAL_ID, AccessibilityEvent.TYPE_VIEW_CLICKED);
+    return result;
+  }
+
+  @Override
+  protected boolean dispatchHoverEvent(MotionEvent event) {
+    return touchHelper.dispatchHoverEvent(event) || super.dispatchHoverEvent(event);
+  }
+
+  private boolean hasCloseIcon() {
+    return chipDrawable != null && chipDrawable.getCloseIcon() != null;
+  }
+
+  private RectF getCloseIconTouchBounds() {
+    rectF.setEmpty();
+
+    if (hasCloseIcon()) {
+      // noinspection ConstantConditions
+      chipDrawable.getCloseIconTouchBounds(rectF);
+    }
+
+    return rectF;
+  }
+
+  private Rect getCloseIconTouchBoundsInt() {
+    RectF bounds = getCloseIconTouchBounds();
+    rect.set((int) bounds.left, (int) bounds.top, (int) bounds.right, (int) bounds.bottom);
+    return rect;
+  }
+
+  /** Provides a virtual view hierarchy for the close icon. */
+  private class ChipTouchHelper extends ExploreByTouchHelper {
+
+    ChipTouchHelper(Chip view) {
+      super(view);
+    }
+
+    @Override
+    protected int getVirtualViewAt(float x, float y) {
+      return (hasCloseIcon() && getCloseIconTouchBounds().contains(x, y))
+          ? CLOSE_ICON_VIRTUAL_ID
+          : HOST_ID;
+    }
+
+    @Override
+    protected void getVisibleVirtualViews(List<Integer> virtualViewIds) {
+      virtualViewIds.add(HOST_ID);
+      if (hasCloseIcon()) {
+        virtualViewIds.add(CLOSE_ICON_VIRTUAL_ID);
+      }
+    }
+
+    @Override
+    protected void onPopulateNodeForVirtualView(
+        int virtualViewId, AccessibilityNodeInfoCompat node) {
+      if (hasCloseIcon()) {
+        node.setContentDescription(
+            getContext().getString(R.string.mtrl_chip_close_icon_content_description));
+        node.setBoundsInParent(getCloseIconTouchBoundsInt());
+        node.addAction(AccessibilityActionCompat.ACTION_CLICK);
+        node.setEnabled(isEnabled());
+      } else {
+        node.setContentDescription("");
+      }
+    }
+
+    @Override
+    protected void onPopulateNodeForHost(AccessibilityNodeInfoCompat node) {
+      node.setCheckable(chipDrawable != null && chipDrawable.isCheckable());
+      node.setClassName(Chip.class.getName());
+      node.setContentDescription(
+          Chip.class.getSimpleName()
+              + ". "
+              + (chipDrawable != null ? chipDrawable.getChipText() : ""));
+    }
+
+    @Override
+    protected boolean onPerformActionForVirtualView(
+        int virtualViewId, int action, Bundle arguments) {
+      if (action == AccessibilityNodeInfoCompat.ACTION_CLICK
+          && virtualViewId == CLOSE_ICON_VIRTUAL_ID) {
+        return performCloseIconClick();
+      }
+      return false;
     }
   }
 }
