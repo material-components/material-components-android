b864c352a182b0757357dd2ec79cfd910a26aa01
dcarlsson <dcarlsson@google.com>
2018-01-06 14:13:43 -0500

    Add class for MaterialCard. Add foundation for styles and theme support.
    PiperOrigin-RevId: 181045762

---
+++ lib/java/android/support/design/card/MaterialCardView.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.support.design.card;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+
+/**
+ * A convenience class for creating a new Material card.
+ *
+ * <p>This class supplies updated Material styles for the card in the constructor. The widget will
+ * display the correct default Material styles without the use of the style flag.
+ */
+public class MaterialCardView extends CardView {
+
+  public MaterialCardView(Context context) {
+    this(context, null /* attrs */);
+  }
+
+  public MaterialCardView(Context context, AttributeSet attrs) {
+    this(context, attrs, android.support.design.card.R.attr.cardViewStyle);
+  }
+
+  public MaterialCardView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+}
