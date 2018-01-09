9c11d3531a622f2565d09fe67bef3d6596f680b8
Material Design Team <noreply@google.com>
2018-01-08 19:43:03 -0500

    Prevent potential NPE. It's reported in live AGSA bugs and the root cause could be the same with cl/152195381.
    PiperOrigin-RevId: 181238092

--- lib/java/android/support/design/widget/BottomSheetBehavior.java
+++ lib/java/android/support/design/widget/BottomSheetBehavior.java
@@ -303,7 +303,7 @@ public class BottomSheetBehavior<V extends View> extends CoordinatorLayout.Behav
     // We have to handle cases that the ViewDragHelper does not capture the bottom sheet because
     // it is not the top most view of its parent. This is not necessary when the touch event is
     // happening over the scrolling content as nested scrolling logic handles that case.
-    View scroll = nestedScrollingChildRef.get();
+    View scroll = nestedScrollingChildRef != null ? nestedScrollingChildRef.get() : null;
     return action == MotionEvent.ACTION_MOVE
         && scroll != null
         && !ignoreEvents
