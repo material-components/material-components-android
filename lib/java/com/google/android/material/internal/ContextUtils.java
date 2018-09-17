dff21575fbbd6a1cbc547b9b4c8f94370219cfa3
dniz <dniz@google.com>
2018-09-17 19:12:55 -0400

    Add ContextUtils getActivity() method
    PiperOrigin-RevId: 213356904

---
+++ lib/java/com/google/android/material/internal/ContextUtils.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.material.internal;
+
+import static android.support.annotation.RestrictTo.Scope.LIBRARY_GROUP;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.support.annotation.Nullable;
+import android.support.annotation.RestrictTo;
+
+/**
+ * Utils class for {@link Context}.
+ *
+ * @hide
+ */
+@RestrictTo(LIBRARY_GROUP)
+public class ContextUtils {
+
+  /**
+   * Returns the {@link Activity} given a {@link Context} or null if there is no {@link Activity},
+   * taking into account the potential hierarchy of {@link ContextWrapper ContextWrappers}.
+   */
+  @Nullable
+  public static Activity getActivity(Context context) {
+    while (context instanceof ContextWrapper) {
+      if (context instanceof Activity) {
+        return (Activity) context;
+      }
+      context = ((ContextWrapper) context).getBaseContext();
+    }
+    return null;
+  }
+}
