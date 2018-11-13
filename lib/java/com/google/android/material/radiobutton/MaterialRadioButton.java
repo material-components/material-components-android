7084feab9fb916952d6d7e81e03ce53d8d007123
isabellekim <isabellekim@google.com>
2018-11-13 15:41:41 -0500

    Support materialThemeOverlay for RadioButton.
    PiperOrigin-RevId: 221315243

--- lib/java/com/google/android/material/radiobutton/MaterialRadioButton.java
+++ lib/java/com/google/android/material/radiobutton/MaterialRadioButton.java
@@ -18,6 +18,8 @@ package com.google.android.material.radiobutton;
 
 import com.google.android.material.R;
 
+import static com.google.android.material.internal.ThemeEnforcement.createThemedContext;
+
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
@@ -37,6 +39,9 @@ import android.util.AttributeSet;
  */
 public class MaterialRadioButton extends AppCompatRadioButton {
 
+  private static final int DEF_STYLE_RES =
+      R.style.Widget_MaterialComponents_CompoundButton_RadioButton;
+
   private final int[][] enabledCheckedStates =
       new int[][] {
         new int[] {android.R.attr.state_enabled, android.R.attr.state_checked}, // [0]
@@ -54,15 +59,13 @@ public class MaterialRadioButton extends AppCompatRadioButton {
   }
 
   public MaterialRadioButton(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-    super(context, attrs, defStyleAttr);
+    super(createThemedContext(context, attrs, defStyleAttr, DEF_STYLE_RES), attrs, defStyleAttr);
+    // Ensure we are using the correctly themed context rather than the context that was passed in.
+    context = getContext();
 
     TypedArray attributes =
         ThemeEnforcement.obtainStyledAttributes(
-            context,
-            attrs,
-            R.styleable.MaterialRadioButton,
-            defStyleAttr,
-            R.style.Widget_MaterialComponents_CompoundButton_RadioButton);
+            context, attrs, R.styleable.MaterialRadioButton, defStyleAttr, DEF_STYLE_RES);
 
     boolean useMaterialThemeColors =
         attributes.getBoolean(R.styleable.MaterialRadioButton_useMaterialThemeColors, false);
