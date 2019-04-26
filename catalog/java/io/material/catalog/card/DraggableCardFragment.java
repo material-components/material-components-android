a47475da0d331ce801e57c08189c2d6d9fe2b267
marianomartin <marianomartin@google.com>
2019-04-26 07:44:29 -0700

    Support drag actions for A11y in draggable card demo
    PiperOrigin-RevId: 245418849

--- catalog/java/io/material/catalog/card/DraggableCardFragment.java
+++ catalog/java/io/material/catalog/card/DraggableCardFragment.java
@@ -18,18 +18,32 @@ package io.material.catalog.card;
 
 import io.material.catalog.R;
 
+import android.animation.LayoutTransition;
+import android.os.Build;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.google.android.material.card.MaterialCardView;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.AccessibilityDelegate;
 import android.view.ViewGroup;
+import android.view.accessibility.AccessibilityNodeInfo;
+import android.view.accessibility.AccessibilityNodeInfo.AccessibilityAction;
 import io.material.catalog.draggable.DraggableCoordinatorLayout;
+import io.material.catalog.draggable.DraggableCoordinatorLayout.ViewDragListener;
 import io.material.catalog.feature.DemoFragment;
 
-/** A fragment with a draggable MaterialCardView */
+/**
+ * A fragment with a draggable MaterialCardView
+ */
 public class DraggableCardFragment extends DemoFragment {
+
+  private MaterialCardView card;
 
   @Override
   public int getDemoTitleResId() {
@@ -43,15 +57,21 @@ public class DraggableCardFragment extends DemoFragment {
         layoutInflater.inflate(
             R.layout.cat_card_draggable_fragment, viewGroup, false /* attachToRoot */);
     DraggableCoordinatorLayout container = (DraggableCoordinatorLayout) view;
-    final MaterialCardView card = view.findViewById(R.id.draggable_card);
+    LayoutTransition transition = ((CoordinatorLayout) view).getLayoutTransition();
+    if (VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN) {
+      transition.enableTransitionType(LayoutTransition.CHANGING);
+    }
+
+    card = view.findViewById(R.id.draggable_card);
+    card.setAccessibilityDelegate(cardDelegate);
     container.addDraggableChild(card);
 
-    if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.LOLLIPOP) {
+    if (VERSION.SDK_INT < VERSION_CODES.LOLLIPOP) {
       return view;
     }
 
     container.setViewDragListener(
-        new DraggableCoordinatorLayout.ViewDragListener() {
+        new ViewDragListener() {
           @Override
           public void onViewCaptured(@NonNull View view, int i) {
             card.setDragged(true);
@@ -65,4 +85,73 @@ public class DraggableCardFragment extends DemoFragment {
 
     return view;
   }
+
+  private final AccessibilityDelegate cardDelegate = new AccessibilityDelegate() {
+    @Override
+    public void onInitializeAccessibilityNodeInfo(View host, AccessibilityNodeInfo info) {
+      super.onInitializeAccessibilityNodeInfo(host, info);
+      if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+        return;
+      }
+
+      CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) card
+          .getLayoutParams();
+      int gravity = layoutParams.gravity;
+      boolean isOnLeft = (gravity & Gravity.LEFT) == Gravity.LEFT;
+      boolean isOnRight = (gravity & Gravity.RIGHT) == Gravity.RIGHT;
+      boolean isOnTop = (gravity & Gravity.TOP) == Gravity.TOP;
+      boolean isOnBottom = (gravity & Gravity.BOTTOM) == Gravity.BOTTOM;
+      boolean isOnCenter = (gravity & Gravity.HORIZONTAL_GRAVITY_MASK) == Gravity.CENTER_HORIZONTAL;
+
+      if (!(isOnTop && isOnLeft)) {
+        info.addAction(new AccessibilityAction(R.id.move_card_top_left_action,
+            getString(R.string.cat_card_action_move_top_left)));
+      }
+      if (!(isOnTop && isOnRight)) {
+        info.addAction(new AccessibilityAction(R.id.move_card_top_right_action,
+            getString(R.string.cat_card_action_move_top_right)));
+      }
+      if (!(isOnBottom && isOnLeft)) {
+        info.addAction(new AccessibilityAction(R.id.move_card_bottom_left_action,
+            getString(R.string.cat_card_action_move_bottom_left)));
+      }
+      if (!(isOnBottom && isOnRight)) {
+        info.addAction(new AccessibilityAction(
+            R.id.move_card_bottom_right_action,
+            getString(R.string.cat_card_action_move_bottom_right)));
+      }
+      if (!isOnCenter) {
+        info.addAction(new AccessibilityAction(
+            R.id.move_card_center_action,
+            getString(R.string.cat_card_action_move_center)));
+      }
+    }
+
+    @Override
+    public boolean performAccessibilityAction(View host, int action, Bundle arguments) {
+      int gravity;
+      if (action == R.id.move_card_top_left_action) {
+        gravity = Gravity.TOP | Gravity.LEFT;
+      } else if (action == R.id.move_card_top_right_action) {
+        gravity = Gravity.TOP | Gravity.RIGHT;
+      } else if (action == R.id.move_card_bottom_left_action) {
+        gravity = Gravity.BOTTOM | Gravity.LEFT;
+      } else if (action == R.id.move_card_bottom_right_action) {
+        gravity = Gravity.BOTTOM | Gravity.RIGHT;
+      } else if (action == R.id.move_card_center_action) {
+        gravity = Gravity.CENTER;
+      } else {
+        return super.performAccessibilityAction(host, action, arguments);
+      }
+
+      CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) card
+          .getLayoutParams();
+      if (layoutParams.gravity != gravity) {
+        layoutParams.gravity = gravity;
+        card.requestLayout();
+      }
+
+      return true;
+    }
+  };
 }
