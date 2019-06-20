5bf8f92e78f83b29f477dc49f072a87189fe5526
isabellekim <isabellekim@google.com>
2019-06-20 12:01:14 -0400

    Update RippleDrawableCompat to use MaterialShapeDrawable as a delegate and implement ConstantState.
    This will make RippleDrawable work with a single-value ColorStateList as previously it getConstantState().newDrawable() would recreate a MaterialShapeDrawable, losing the logic in RippleDrawableCompat.
    
    PiperOrigin-RevId: 254207262

--- lib/java/com/google/android/material/ripple/RippleDrawableCompat.java
+++ lib/java/com/google/android/material/ripple/RippleDrawableCompat.java
@@ -16,15 +16,25 @@
 
 package com.google.android.material.ripple;
 
+import android.content.res.ColorStateList;
 import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RestrictTo;
 import androidx.annotation.RestrictTo.Scope;
 import com.google.android.material.shape.MaterialShapeDrawable;
 import com.google.android.material.shape.ShapeAppearanceModel;
+import com.google.android.material.shape.Shapeable;
+import androidx.core.graphics.drawable.TintAwareDrawable;
 
 /**
- * A compat {@link android.graphics.drawable.Drawable} to be used pre-Lollipop for drawing an
- * overlay on top of a background for pressed, focused, and hovered states.
+ * A compat {@link Drawable} that is used to provide an overlay for pressed, focused, and hovered
+ * states (only when in enabled). This is intended to be used pre-Lollipop.
  *
  * <p>This Drawable is a {@link MaterialShapeDrawable} so that it can be shaped to match a
  * MaterialShapeDrawable background.
@@ -34,35 +44,152 @@ import com.google.android.material.shape.ShapeAppearanceModel;
  * animation for the pressed state.
  */
 @RestrictTo(Scope.LIBRARY_GROUP)
-public class RippleDrawableCompat extends MaterialShapeDrawable {
+public class RippleDrawableCompat extends Drawable implements Shapeable, TintAwareDrawable {
+
+  private RippleDrawableCompatState drawableState;
 
   /**
-   * Whether this compat ripple should be drawn. True when enabled and (pressed, focused, or
-   * enabled).
-   */
-  private boolean shouldDrawRipple = false;
-
-  /**
-   * Creates a {@link RippleDrawableCompat} with the given shape.
+   * Creates a {@link RippleDrawableCompat} with the given shape that will only draw when enabled
+   * and at leaste one of: presssed, focused, or hovered.
    *
-   * @param shapeAppearanceModel the {@link ShapeAppearanceModel} containing the path that will be
-   *     rendered in this drawable.
+   * @param shapeAppearanceModel The shape for the ripple.
    */
   public RippleDrawableCompat(ShapeAppearanceModel shapeAppearanceModel) {
-    super(shapeAppearanceModel);
+    this(new RippleDrawableCompatState(new MaterialShapeDrawable(shapeAppearanceModel)));
+  }
+
+  private RippleDrawableCompat(RippleDrawableCompatState state) {
+    super();
+    this.drawableState = state;
   }
 
   @Override
-  public void draw(Canvas canvas) {
-    if (shouldDrawRipple) {
-      super.draw(canvas);
-    }
+  public void setTint(@ColorInt int tintColor) {
+    drawableState.delegate.setTint(tintColor);
+  }
+
+  @Override
+  public void setTintMode(@Nullable PorterDuff.Mode tintMode) {
+    drawableState.delegate.setTintMode(tintMode);
+  }
+
+  @Override
+  public void setTintList(@Nullable ColorStateList tintList) {
+    drawableState.delegate.setTintList(tintList);
+  }
+
+  @Override
+  public void setShapeAppearanceModel(@NonNull ShapeAppearanceModel shapeAppearanceModel) {
+    drawableState.delegate.setShapeAppearanceModel(shapeAppearanceModel);
+  }
+
+  /**
+   * Get the {@link ShapeAppearanceModel} containing the path that will be rendered in this
+   * drawable.
+   *
+   * @return the current model.
+   */
+  @Override
+  @NonNull
+  public ShapeAppearanceModel getShapeAppearanceModel() {
+    return drawableState.delegate.getShapeAppearanceModel();
+  }
+
+  /*
+   * This is always stateful as it draws on the canvas only when enabled and (pressed, focused, or
+   * hovered).
+   */
+  @Override
+  public boolean isStateful() {
+    return true;
   }
 
   @Override
   protected boolean onStateChange(int[] stateSet) {
-    final boolean changed = super.onStateChange(stateSet);
-    shouldDrawRipple = RippleUtils.shouldDrawRippleCompat(stateSet);
+    boolean changed = super.onStateChange(stateSet);
+    if (drawableState.delegate.setState(stateSet)) {
+      changed = true;
+    }
+    boolean shouldDrawRipple = RippleUtils.shouldDrawRippleCompat(stateSet);
+    // If shouldDrawRipple is changing, this needs to be redrawn even if the paint / tint values
+    // are not changing in order to support setting a ColorStateList with a single color.
+    if (drawableState.shouldDrawDelegate != shouldDrawRipple) {
+      drawableState.shouldDrawDelegate = shouldDrawRipple;
+      changed = true;
+    }
     return changed;
+  }
+
+  @Override
+  public void draw(Canvas canvas) {
+    // Only draw the delegate Drawable when enabled and at least one of: pressed, focused, hovered.
+    if (drawableState.shouldDrawDelegate) {
+      drawableState.delegate.draw(canvas);
+    }
+  }
+
+  @Override
+  protected void onBoundsChange(Rect bounds) {
+    super.onBoundsChange(bounds);
+    drawableState.delegate.setBounds(bounds);
+  }
+
+  @Nullable
+  @Override
+  public ConstantState getConstantState() {
+    return drawableState;
+  }
+
+  @NonNull
+  @Override
+  public RippleDrawableCompat mutate() {
+    RippleDrawableCompatState newDrawableState = new RippleDrawableCompatState(drawableState);
+    drawableState = newDrawableState;
+    return this;
+  }
+
+  @Override
+  public void setAlpha(int alpha) {
+    drawableState.delegate.setAlpha(alpha);
+  }
+
+  @Override
+  public void setColorFilter(@Nullable ColorFilter colorFilter) {
+    drawableState.delegate.setColorFilter(colorFilter);
+  }
+
+  @Override
+  public int getOpacity() {
+    return drawableState.delegate.getOpacity();
+  }
+
+  /**
+   * A {@link ConstantState} for {@link Ripple}
+   *
+   */
+  static final class RippleDrawableCompatState extends ConstantState {
+
+    @NonNull MaterialShapeDrawable delegate;
+    boolean shouldDrawDelegate;
+
+    public RippleDrawableCompatState(MaterialShapeDrawable delegate) {
+      this.delegate = delegate;
+      this.shouldDrawDelegate = false;
+    }
+
+    public RippleDrawableCompatState(RippleDrawableCompatState orig) {
+      this.delegate = (MaterialShapeDrawable) orig.delegate.getConstantState().newDrawable();
+      this.shouldDrawDelegate = orig.shouldDrawDelegate;
+    }
+
+    @Override
+    public RippleDrawableCompat newDrawable() {
+      return new RippleDrawableCompat(new RippleDrawableCompatState(this));
+    }
+
+    @Override
+    public int getChangingConfigurations() {
+      return 0;
+    }
   }
 }
