14a811d5bd5c211ab16378e14b57b9c956e9927c
Material Design Team <noreply@google.com>
2019-07-09 11:30:07 -0400

    Migrate org.mockito.Matchers#any* to org.mockito.ArgumentMatchers
    The former is deprecated and replaced by the latter in Mockito 2. However, there is a
    functional difference: ArgumentMatchers will reject `null` and check the type
    if the matcher specified a type (e.g. `any(Class)` or `anyInt()`). `any()` will
    remain to accept anything.
    
    PiperOrigin-RevId: 257198124

--- tests/javatests/com/google/android/material/tabs/TabLayoutTest.java
+++ tests/javatests/com/google/android/material/tabs/TabLayoutTest.java
@@ -26,7 +26,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
