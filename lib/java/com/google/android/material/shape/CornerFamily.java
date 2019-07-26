7241e7181f45da70a3725c44ba3aad3c7f5ea4e4
afohrman <afohrman@google.com>
2019-07-26 10:56:27 -0400

    Hide CornerFamily documentation and restrict the annotation.
    PiperOrigin-RevId: 260144206

--- lib/java/com/google/android/material/shape/CornerFamily.java
+++ lib/java/com/google/android/material/shape/CornerFamily.java
@@ -17,6 +17,8 @@
 package com.google.android.material.shape;
 
 import androidx.annotation.IntDef;
+import androidx.annotation.RestrictTo;
+import androidx.annotation.RestrictTo.Scope;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
@@ -27,7 +29,10 @@ import java.lang.annotation.RetentionPolicy;
  * the CornerFamily to {@link CornerFamily#ROUNDED} sets the corner treatment to {@link
  * RoundedCornerTreatment}, and setting the CornerFamily to {@link CornerFamily#CUT} sets the corner
  * treatment to a {@link CutCornerTreatment}.
+ *
+ * @hide
  */
+@RestrictTo(Scope.LIBRARY_GROUP)
 @IntDef({CornerFamily.ROUNDED, CornerFamily.CUT})
 @Retention(RetentionPolicy.SOURCE)
 public @interface CornerFamily {
