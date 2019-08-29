9ed90216a33d77bf5a205f90993ecb544827aae9
Connie <38438920+wcshi@users.noreply.github.com>
2019-08-29 15:55:46 -0400

    Revert "Fixed issue 531 by removing unnecessary calls to set compound drawables"
    This reverts commit faf60b15bea73588e8c76fc348ace50dd430cf55.

--- lib/java/com/google/android/material/button/MaterialButton.java
+++ lib/java/com/google/android/material/button/MaterialButton.java
@@ -663,18 +663,7 @@ public class MaterialButton extends AppCompatButton implements Checkable, Shapea
       icon.setBounds(iconLeft, 0, iconLeft + width, height);
     }
 
-    // Only update if the icon or the position has changed
-    boolean iconStart = iconGravity == ICON_GRAVITY_START || iconGravity == ICON_GRAVITY_TEXT_START;
-    Drawable[] existingDrawables = TextViewCompat.getCompoundDrawablesRelative(this);
-    Drawable drawableStart = existingDrawables[0];
-    Drawable drawableEnd = existingDrawables[2];
-    boolean hasIconChanged = iconStart && drawableStart != icon || !iconStart && drawableEnd != icon;
-
-    if(!hasIconChanged) {
-      return;
-    }
-
-    if (iconStart) {
+    if (iconGravity == ICON_GRAVITY_START || iconGravity == ICON_GRAVITY_TEXT_START) {
       TextViewCompat.setCompoundDrawablesRelative(this, icon, null, null, null);
     } else {
       TextViewCompat.setCompoundDrawablesRelative(this, null, null, icon, null);
