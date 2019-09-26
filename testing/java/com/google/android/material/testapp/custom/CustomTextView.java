e0a5e3ec937288f15c04c236cc414d50ba8148af
Material Design Team <noreply@google.com>
2019-09-26 17:54:30 -0400

    Migrate away from deprecated behavior annotation
    PiperOrigin-RevId: 271439031

--- testing/java/com/google/android/material/testapp/custom/CustomTextView.java
+++ testing/java/com/google/android/material/testapp/custom/CustomTextView.java
@@ -16,12 +16,17 @@
 package com.google.android.material.testapp.custom;
 
 import android.content.Context;
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatTextView;
 import android.util.AttributeSet;
+import android.widget.TextView;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
-@CoordinatorLayout.DefaultBehavior(TestFloatingBehavior.class)
-public class CustomTextView extends AppCompatTextView {
+/**
+ * Custom extension of AppCompat's text view for testing a runtime-specified behavior.
+ */
+public class CustomTextView extends AppCompatTextView
+    implements CoordinatorLayout.AttachedBehavior {
   public CustomTextView(Context context) {
     super(context);
   }
@@ -32,5 +37,11 @@ public class CustomTextView extends AppCompatTextView {
 
   public CustomTextView(Context context, AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
+  }
+
+  @Override
+  @NonNull
+  public CoordinatorLayout.Behavior<TextView> getBehavior() {
+      return new TestFloatingBehavior();
   }
 }
