185fec8087c2a9c7186a2565330c797dd994dfb8
Mattia Pagini <mp.android.develop@gmail.com>
2019-10-10 18:04:17 +0200

    Add test for getCheckedChipIds()

--- lib/javatests/com/google/android/material/chip/ChipGroupTest.java
+++ lib/javatests/com/google/android/material/chip/ChipGroupTest.java
@@ -29,6 +29,8 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.internal.DoNotInstrument;
 
+import java.util.List;
+
 /** Tests for {@link com.google.android.material.chip.ChipGroup}. */
 @RunWith(RobolectricTestRunner.class)
 @DoNotInstrument
@@ -64,5 +66,26 @@ public class ChipGroupTest {
     assertThat(chipId).isEqualTo(chipgroup.getCheckedChipId());
     chipgroup.clearCheck();
     assertThat(chipgroup.getCheckedChipId()).isEqualTo(View.NO_ID);
+  }
+
+  @Test
+  public void testMultipleCheckedChip() {
+    int chip1Id = chipgroup.getChildAt(0).getId();
+    chipgroup.check(chip1Id);
+    int chip2Id = chipgroup.getChildAt(1).getId();
+    chipgroup.check(chip2Id);
+    assertThat(chipgroup.getCheckedChipIds()).hasSize(2);
+  }
+
+  @Test
+  public void testSingleCheckedChip() {
+    chipgroup.setSingleSelection(true);
+    int chipId = chipgroup.getChildAt(2).getId();
+    chipgroup.check(chipId);
+    assertThat(chipgroup.getCheckedChipIds()).hasSize(1);
+    //check that for a single checked chip methods should be equivalent
+    Integer checkedId1 = chipgroup.getCheckedChipIds().get(0);
+    int checkedId2 = chipgroup.getCheckedChipId();
+    assertThat(checkedId1).isEqualTo(checkedId2);
   }
 }
