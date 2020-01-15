6c4138803c77030d57c6ab5da643921e78d640da
marianomartin <marianomartin@google.com>
2020-01-15 04:06:35 -0800

    Bring back removed TabLayout method for compatibility
    Resolves https://github.com/material-components/material-components-android/issues/903
    
    We accidentally removed this since it wasn't in 1.0.0 and it was deprecated. It exists to keep compatibility with version 27.0 of the library.
    
    PiperOrigin-RevId: 289832106
    (cherry picked from commit 17912e05993e6d4bef27c1db738af0e71f94ef29)

--- lib/java/com/google/android/material/tabs/TabLayout.java
+++ lib/java/com/google/android/material/tabs/TabLayout.java
@@ -432,6 +432,8 @@ public class TabLayout extends HorizontalScrollView {
   boolean tabIndicatorFullWidth;
   boolean unboundedRipple;
 
+  @Nullable private OnTabSelectedListener selectedListener;
+
   private final ArrayList<OnTabSelectedListener> selectedListeners = new ArrayList<>();
   @Nullable private OnTabSelectedListener currentVpSelectedListener;
   private final HashMap<BaseOnTabSelectedListener<? extends Tab>, OnTabSelectedListener>
@@ -719,6 +721,25 @@ public class TabLayout extends HorizontalScrollView {
       tab.setContentDescription(item.getContentDescription());
     }
     addTab(tab);
+  }
+
+  /**
+   * @deprecated Use {@link #addOnTabSelectedListener(OnTabSelectedListener)} and {@link
+   *    #removeOnTabSelectedListener(OnTabSelectedListener)}.
+   */
+  @Deprecated
+  public void setOnTabSelectedListener(@Nullable OnTabSelectedListener listener) {
+    // The logic in this method emulates what we had before support for multiple
+    // registered listeners.
+    if (selectedListener != null) {
+      removeOnTabSelectedListener(selectedListener);
+    }
+    // Update the deprecated field so that we can remove the passed listener the next
+    // time we're called
+    selectedListener = listener;
+    if (listener != null) {
+      addOnTabSelectedListener(listener);
+    }
   }
 
   /**
