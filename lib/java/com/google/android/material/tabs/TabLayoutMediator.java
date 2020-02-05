c859bf556c0ed33250f717276f0f06094ef1030f
vipulasri <vipulasri.2007@gmail.com>
2020-02-05 13:27:45 -0500

    [TabLayout] added smooth scroll functionality in TabLayoutMediator
    Resolves https://github.com/material-components/material-components-android/pull/951
    
    GIT_ORIGIN_REV_ID=23c7ef34682c2eb55fcb18db677c54f006e3cb15
    PiperOrigin-RevId: 293397927

--- lib/java/com/google/android/material/tabs/TabLayoutMediator.java
+++ lib/java/com/google/android/material/tabs/TabLayoutMediator.java
@@ -47,6 +47,7 @@ public final class TabLayoutMediator {
   @NonNull private final TabLayout tabLayout;
   @NonNull private final ViewPager2 viewPager;
   private final boolean autoRefresh;
+  private final boolean smoothScroll;
   private final TabConfigurationStrategy tabConfigurationStrategy;
   @Nullable private RecyclerView.Adapter<?> adapter;
   private boolean attached;
@@ -75,7 +76,7 @@ public final class TabLayoutMediator {
       @NonNull TabLayout tabLayout,
       @NonNull ViewPager2 viewPager,
       @NonNull TabConfigurationStrategy tabConfigurationStrategy) {
-    this(tabLayout, viewPager, true, tabConfigurationStrategy);
+    this(tabLayout, viewPager, /* autoRefresh= */ true, tabConfigurationStrategy);
   }
 
   public TabLayoutMediator(
@@ -83,9 +84,19 @@ public final class TabLayoutMediator {
       @NonNull ViewPager2 viewPager,
       boolean autoRefresh,
       @NonNull TabConfigurationStrategy tabConfigurationStrategy) {
+    this(tabLayout, viewPager, autoRefresh, /* smoothScroll= */ true, tabConfigurationStrategy);
+  }
+
+  public TabLayoutMediator(
+      @NonNull TabLayout tabLayout,
+      @NonNull ViewPager2 viewPager,
+      boolean autoRefresh,
+      boolean smoothScroll,
+      @NonNull TabConfigurationStrategy tabConfigurationStrategy) {
     this.tabLayout = tabLayout;
     this.viewPager = viewPager;
     this.autoRefresh = autoRefresh;
+    this.smoothScroll = smoothScroll;
     this.tabConfigurationStrategy = tabConfigurationStrategy;
   }
 
@@ -113,7 +124,7 @@ public final class TabLayoutMediator {
     viewPager.registerOnPageChangeCallback(onPageChangeCallback);
 
     // Now we'll add a tab selected listener to set ViewPager's current item
-    onTabSelectedListener = new ViewPagerOnTabSelectedListener(viewPager);
+    onTabSelectedListener = new ViewPagerOnTabSelectedListener(viewPager, smoothScroll);
     tabLayout.addOnTabSelectedListener(onTabSelectedListener);
 
     // Now we'll populate ourselves from the pager adapter, adding an observer if
@@ -238,14 +249,16 @@ public final class TabLayoutMediator {
    */
   private static class ViewPagerOnTabSelectedListener implements TabLayout.OnTabSelectedListener {
     private final ViewPager2 viewPager;
+    private final boolean smoothScroll;
 
-    ViewPagerOnTabSelectedListener(ViewPager2 viewPager) {
+    ViewPagerOnTabSelectedListener(ViewPager2 viewPager, boolean smoothScroll) {
       this.viewPager = viewPager;
+      this.smoothScroll = smoothScroll;
     }
 
     @Override
     public void onTabSelected(@NonNull TabLayout.Tab tab) {
-      viewPager.setCurrentItem(tab.getPosition(), true);
+      viewPager.setCurrentItem(tab.getPosition(), smoothScroll);
     }
 
     @Override
