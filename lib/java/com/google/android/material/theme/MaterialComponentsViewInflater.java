4fb2375688bba25168791191910c86602636ab50
connieshi <connieshi@google.com>
2020-04-16 10:24:53 -0400

    Remove AppCompatButton workaround for FloatingToolbar in API 23-25 now that MaterialButton supports android:background.
    PiperOrigin-RevId: 306841859

--- lib/java/com/google/android/material/theme/MaterialComponentsViewInflater.java
+++ lib/java/com/google/android/material/theme/MaterialComponentsViewInflater.java
@@ -16,14 +16,9 @@
 
 package com.google.android.material.theme;
 
-import static androidx.annotation.RestrictTo.Scope.LIBRARY_GROUP;
-
 import android.content.Context;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RestrictTo;
 import androidx.appcompat.app.AppCompatViewInflater;
 import androidx.appcompat.widget.AppCompatAutoCompleteTextView;
 import androidx.appcompat.widget.AppCompatButton;
@@ -42,54 +37,10 @@ import com.google.android.material.textview.MaterialTextView;
  * Components ones at inflation time, provided a Material Components theme is in use.
  */
 public class MaterialComponentsViewInflater extends AppCompatViewInflater {
-
-  // Cached background resource ID used for workaround to not inflate MaterialButton in
-  // API 23-25 FloatingToolbar. Technically 0 is the only invalid resource ID, but we are assuming
-  // it's safe to use -1 as a sentinel here.
-  private static int floatingToolbarItemBackgroundResId = -1;
-
   @NonNull
   @Override
   protected AppCompatButton createButton(@NonNull Context context, @NonNull AttributeSet attrs) {
-    if (shouldInflateAppCompatButton(context, attrs)) {
-      return new AppCompatButton(context, attrs);
-    }
-
     return new MaterialButton(context, attrs);
-  }
-
-  /** @hide */
-  @RestrictTo(LIBRARY_GROUP)
-  protected boolean shouldInflateAppCompatButton(
-      @NonNull Context context, @NonNull AttributeSet attrs) {
-    // Workaround for FloatingToolbar inflating floating_popup_menu_button.xml on API 23-25, which
-    // should not have MaterialButton styling.
-
-    if (!(VERSION.SDK_INT == VERSION_CODES.M
-        || VERSION.SDK_INT == VERSION_CODES.N
-        || VERSION.SDK_INT == VERSION_CODES.N_MR1)) {
-      return false;
-    }
-
-    if (floatingToolbarItemBackgroundResId == -1) {
-      floatingToolbarItemBackgroundResId =
-          context
-              .getResources()
-              .getIdentifier("floatingToolbarItemBackgroundDrawable", "^attr-private", "android");
-    }
-
-    if (floatingToolbarItemBackgroundResId != 0 && floatingToolbarItemBackgroundResId != -1) {
-      for (int i = 0; i < attrs.getAttributeCount(); i++) {
-        if (attrs.getAttributeNameResource(i) == android.R.attr.background) {
-          int backgroundResourceId = attrs.getAttributeListValue(i, null, 0);
-          if (floatingToolbarItemBackgroundResId == backgroundResourceId) {
-            return true;
-          }
-        }
-      }
-    }
-
-    return false;
   }
 
   @NonNull
