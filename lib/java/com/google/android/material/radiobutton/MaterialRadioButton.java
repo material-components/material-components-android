c6202adbe249d8a550113d397b538f3253c1ee06
Frantisek Nagy <frantisek.nagy.ml@gmail.com>
2020-04-20 16:52:06 +0200

    Fix MaterialRadioButton tint from theme attributes on api 21 + add test

--- lib/java/com/google/android/material/radiobutton/MaterialRadioButton.java
+++ lib/java/com/google/android/material/radiobutton/MaterialRadioButton.java
@@ -30,6 +30,7 @@ import androidx.appcompat.widget.AppCompatRadioButton;
 import android.util.AttributeSet;
 import com.google.android.material.color.MaterialColors;
 import com.google.android.material.internal.ThemeEnforcement;
+import com.google.android.material.resources.MaterialResources;
 
 /**
  * A class that creates a Material Themed RadioButton.
@@ -70,6 +71,15 @@ public class MaterialRadioButton extends AppCompatRadioButton {
     TypedArray attributes =
         ThemeEnforcement.obtainStyledAttributes(
             context, attrs, R.styleable.MaterialRadioButton, defStyleAttr, DEF_STYLE_RES);
+
+    // If buttonTint is specified, read it using MaterialResources to allow themeable attributes in
+    // all API levels.
+    if (attributes.hasValue(R.styleable.MaterialRadioButton_buttonTint)) {
+      CompoundButtonCompat.setButtonTintList(
+          this,
+          MaterialResources.getColorStateList(
+              context, attributes, R.styleable.MaterialRadioButton_buttonTint));
+    }
 
     useMaterialThemeColors =
         attributes.getBoolean(R.styleable.MaterialRadioButton_useMaterialThemeColors, false);
