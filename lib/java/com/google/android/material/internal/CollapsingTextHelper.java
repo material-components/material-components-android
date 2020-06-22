material-components/material-components-android

commit 83e4e47d2225cd281bef195d1baf035117cda4cd

Initial support for Multiline CollapsingToolbarLayout
This cl adds the capabilities to set multiline on CollapsingToolbarLayout while keeping single line toolbars unchanged.

Co-Authored-By: johan12345 <johan.forstner@gmail.com>
PiperOrigin-RevId: 294256516

lib/java/com/google/android/material/internal/CollapsingTextHelper.java
@@ -63,6 +63,7 @@ public final class CollapsingTextHelper {
   // by using our own texture
   private static final boolean USE_SCALING_TEXTURE = Build.VERSION.SDK_INT < 18;
   private static final String TAG = "CollapsingTextHelper";
+  private static final String ELLIPSIS_NORMAL = "\u2026"; // HORIZONTAL ELLIPSIS (…)
 
   private static final boolean DEBUG_DRAW = false;
   @NonNull private static final Paint DEBUG_DRAW_PAINT;
@@ -133,6 +134,11 @@ public final class CollapsingTextHelper {
   private float expandedShadowDy;
   private ColorStateList expandedShadowColor;
   private StaticLayout textLayout;
+  private float collapsedTextBlend;
+  private float expandedTextBlend;
+  private float expandedFirstLineDrawX;
+  private CharSequence textToDrawCollapsed;
+  private int maxLines = 1;
 
   public CollapsingTextHelper(View view) {
     this.view = view;
@@ -278,9 +284,6 @@ public final class CollapsingTextHelper {
   }
 
   public void setExpandedTextGravity(int gravity) {
-    if ((gravity & GravityCompat.RELATIVE_HORIZONTAL_GRAVITY_MASK) == 0) {
-      gravity |= GravityCompat.START;
-    }
     if (expandedTextGravity != gravity) {
       expandedTextGravity = gravity;
       recalculate();
@@ -292,9 +295,6 @@ public final class CollapsingTextHelper {
   }
 
   public void setCollapsedTextGravity(int gravity) {
-    if ((gravity & GravityCompat.RELATIVE_HORIZONTAL_GRAVITY_MASK) == 0) {
-      gravity |= GravityCompat.START;
-    }
     if (collapsedTextGravity != gravity) {
       collapsedTextGravity = gravity;
       recalculate();
@@ -484,6 +484,10 @@ public final class CollapsingTextHelper {
     setInterpolatedTextSize(
         lerp(expandedTextSize, collapsedTextSize, fraction, textSizeInterpolator));
 
+    setCollapsedTextBlend(
+        1 - lerp(0, 1, 1 - fraction, AnimationUtils.FAST_OUT_SLOW_IN_INTERPOLATOR));
+    setExpandedTextBlend(lerp(1, 0, fraction, AnimationUtils.FAST_OUT_SLOW_IN_INTERPOLATOR));
+
     if (collapsedTextColor != expandedTextColor) {
       // If the collapsed and expanded text colors are different, blend them based on the
       // fraction
@@ -529,12 +533,21 @@ public final class CollapsingTextHelper {
 
     // We then calculate the collapsed text size, using the same logic
     calculateUsingTextSize(collapsedTextSize);
+    if (textToDraw != null && textLayout != null) {
+      textToDrawCollapsed =
+          TextUtils.ellipsize(textToDraw, textPaint, textLayout.getWidth(), TruncateAt.END);
+    }
     float width =
-        textToDraw != null ? textPaint.measureText(textToDraw, 0, textToDraw.length()) : 0;
+        textToDrawCollapsed != null
+            ? textPaint.measureText(textToDrawCollapsed, 0, textToDrawCollapsed.length())
+            : 0;
     final int collapsedAbsGravity =
         GravityCompat.getAbsoluteGravity(
             collapsedTextGravity,
             isRtl ? ViewCompat.LAYOUT_DIRECTION_RTL : ViewCompat.LAYOUT_DIRECTION_LTR);
+
+    float textLayoutHeight = textLayout != null ? textLayout.getHeight() : 0;
+
     switch (collapsedAbsGravity & Gravity.VERTICAL_GRAVITY_MASK) {
       case Gravity.BOTTOM:
         collapsedDrawY = collapsedBounds.bottom;
@@ -546,9 +559,12 @@ public final class CollapsingTextHelper {
       default:
         float textHeight = textPaint.descent() - textPaint.ascent();
         float textOffset = (textHeight / 2) - textPaint.descent();
-        collapsedDrawY = collapsedBounds.centerY() + textOffset;
+        collapsedDrawY = shouldDrawMultiline()
+            ? collapsedBounds.centerY() - (textHeight / 2)
+            : collapsedBounds.centerY() + textOffset;
         break;
     }
+
     switch (collapsedAbsGravity & GravityCompat.RELATIVE_HORIZONTAL_GRAVITY_MASK) {
       case Gravity.CENTER_HORIZONTAL:
         collapsedDrawX = collapsedBounds.centerX() - (width / 2);
@@ -563,14 +579,22 @@ public final class CollapsingTextHelper {
     }
 
     calculateUsingTextSize(expandedTextSize);
-    width = textToDraw != null ? textPaint.measureText(textToDraw, 0, textToDraw.length()) : 0;
+
+    float measuredWidth = textToDraw != null
+        ? textPaint.measureText(textToDraw, 0, textToDraw.length()) : 0;
+    width = textLayout != null && maxLines > 1 && !isRtl
+        ? textLayout.getLineWidth(0)
+        : measuredWidth;
+    expandedFirstLineDrawX = textLayout != null ? textLayout.getLineLeft(0) : 0;
+
     final int expandedAbsGravity =
         GravityCompat.getAbsoluteGravity(
             expandedTextGravity,
             isRtl ? ViewCompat.LAYOUT_DIRECTION_RTL : ViewCompat.LAYOUT_DIRECTION_LTR);
     switch (expandedAbsGravity & Gravity.VERTICAL_GRAVITY_MASK) {
       case Gravity.BOTTOM:
-        expandedDrawY = expandedBounds.bottom;
+        float offset = shouldDrawMultiline() ? textLayoutHeight - textPaint.descent() : 0;
+        expandedDrawY = expandedBounds.bottom - offset;
         break;
       case Gravity.TOP:
         expandedDrawY = expandedBounds.top - textPaint.ascent();
@@ -579,9 +603,12 @@ public final class CollapsingTextHelper {
       default:
         float textHeight = textPaint.descent() - textPaint.ascent();
         float textOffset = (textHeight / 2) - textPaint.descent();
-        expandedDrawY = expandedBounds.centerY() + textOffset;
+        expandedDrawY = shouldDrawMultiline()
+            ? expandedBounds.centerY() - (textLayoutHeight / 2)
+            : expandedBounds.centerY() + textOffset;
         break;
     }
+
     switch (expandedAbsGravity & GravityCompat.RELATIVE_HORIZONTAL_GRAVITY_MASK) {
       case Gravity.CENTER_HORIZONTAL:
         expandedDrawX = expandedBounds.centerX() - (width / 2);
@@ -611,13 +638,26 @@ public final class CollapsingTextHelper {
         lerp(expandedBounds.bottom, collapsedBounds.bottom, fraction, positionInterpolator);
   }
 
+  private void setCollapsedTextBlend(float blend) {
+    collapsedTextBlend = blend;
+    ViewCompat.postInvalidateOnAnimation(view);
+  }
+
+  private void setExpandedTextBlend(float blend) {
+    expandedTextBlend = blend;
+    ViewCompat.postInvalidateOnAnimation(view);
+  }
+
   public void draw(@NonNull Canvas canvas) {
     final int saveCount = canvas.save();
-
+    // Compute where to draw textLayout for this frame
     if (textToDraw != null && drawTitle) {
+      final float currentExpandedX =
+          currentDrawX + textLayout.getLineLeft(0) - expandedFirstLineDrawX * 2;
+
+      textPaint.setTextSize(currentTextSize);
       float x = currentDrawX;
       float y = currentDrawY;
-
       final boolean drawTexture = useTexture && expandedTitleTexture != null;
 
       final float ascent = textLayout.getLineAscent(0);
@@ -639,13 +679,50 @@ public final class CollapsingTextHelper {
       if (drawTexture) {
         // If we should use a texture, draw it instead of text
         canvas.drawBitmap(expandedTitleTexture, x, y + ascent, texturePaint);
+        canvas.restoreToCount(saveCount);
+        return;
+      }
+
+      if (shouldDrawMultiline()) {
+        drawMultinlineTransition(canvas, currentExpandedX, x, y, ascent);
       } else {
         canvas.translate(x, y + ascent);
         textLayout.draw(canvas);
       }
-    }
 
-    canvas.restoreToCount(saveCount);
+      canvas.restoreToCount(saveCount);
+    }
+  }
+
+  private boolean shouldDrawMultiline() {
+    return maxLines > 1 && !isRtl && !useTexture;
+  }
+
+  private void drawMultinlineTransition(
+      @NonNull Canvas canvas, float currentExpandedX, float x, float y, float ascent) {
+    int originalAlpha = textPaint.getAlpha();
+    // positon expanded text appropriately
+    canvas.translate(currentExpandedX, y);
+    // Expanded text
+    textPaint.setAlpha((int) (expandedTextBlend * originalAlpha));
+    textLayout.draw(canvas);
+
+    // position the overlays
+    canvas.translate(x - currentExpandedX, 0);
+
+    // Collapsed text
+    textPaint.setAlpha((int) (collapsedTextBlend * originalAlpha));
+    canvas.drawText(
+        textToDrawCollapsed, 0, textToDrawCollapsed.length(), 0, -ascent / scale, textPaint);
+    // Remove ellipsis for Cross-section animation
+    String tmp = textToDrawCollapsed.toString().trim();
+    if (tmp.endsWith(ELLIPSIS_NORMAL)) {
+      tmp = tmp.substring(0, tmp.length() - 1);
+    }
+    // Cross-section between both texts (should stay at original alpha)
+    textPaint.setAlpha(originalAlpha);
+    canvas.drawText(
+        tmp, 0, Math.min(textLayout.getLineEnd(0), tmp.length()), 0, -ascent / scale, textPaint);
   }
 
   private boolean calculateIsRtl(@NonNull CharSequence text) {
@@ -686,7 +763,6 @@ public final class CollapsingTextHelper {
     float availableWidth;
     float newTextSize;
     boolean updateDrawText = false;
-    int maxLines = 1;
 
     if (isClose(textSize, collapsedTextSize)) {
       newTextSize = collapsedTextSize;
@@ -738,7 +814,7 @@ public final class CollapsingTextHelper {
       textPaint.setLinearText(scale != 1f);
 
       isRtl = calculateIsRtl(text);
-      textLayout = createStaticLayout(maxLines, availableWidth, isRtl);
+      textLayout = createStaticLayout(shouldDrawMultiline() ? maxLines : 1, availableWidth, isRtl);
       textToDraw = textLayout.getText();
     }
   }
@@ -817,6 +893,18 @@ public final class CollapsingTextHelper {
       expandedTitleTexture.recycle();
       expandedTitleTexture = null;
     }
+  }
+
+  public void setMaxLines(int maxLines) {
+    if (maxLines != this.maxLines) {
+      this.maxLines = maxLines;
+      clearTexture();
+      recalculate();
+    }
+  }
+
+  public int getMaxLines() {
+    return maxLines;
   }
 
   /**

lib/java/com/google/android/material/appbar/CollapsingToolbarLayout.java
@@ -19,7 +19,6 @@ package com.google.android.material.appbar;
 import com.google.android.material.R;
 
 import static androidx.annotation.RestrictTo.Scope.LIBRARY_GROUP;
-import static com.google.android.material.theme.overlay.MaterialThemeOverlay.wrap;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
@@ -112,7 +111,6 @@ import java.lang.annotation.RetentionPolicy;
  */
 public class CollapsingToolbarLayout extends FrameLayout {
 
-  private static final int DEF_STYLE_RES = R.style.Widget_Design_CollapsingToolbar;
   private static final int DEFAULT_SCRIM_ANIMATION_DURATION = 600;
 
   private boolean refreshToolbar = true;
@@ -154,9 +152,7 @@ public class CollapsingToolbarLayout extends FrameLayout {
   }
 
   public CollapsingToolbarLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-    super(wrap(context, attrs, defStyleAttr, DEF_STYLE_RES), attrs, defStyleAttr);
-    // Ensure we are using the correctly themed context rather than the context that was passed in.
-    context = getContext();
+    super(context, attrs, defStyleAttr);
 
     collapsingTextHelper = new CollapsingTextHelper(this);
     collapsingTextHelper.setTextSizeInterpolator(AnimationUtils.DECELERATE_INTERPOLATOR);
@@ -167,7 +163,7 @@ public class CollapsingToolbarLayout extends FrameLayout {
             attrs,
             R.styleable.CollapsingToolbarLayout,
             defStyleAttr,
-            DEF_STYLE_RES);
+            R.style.Widget_Design_CollapsingToolbar);
 
     collapsingTextHelper.setExpandedTextGravity(
         a.getInt(
@@ -1048,6 +1044,24 @@ public class CollapsingToolbarLayout extends FrameLayout {
   }
 
   /**
+   * Sets the maximum number of lines to display in the expanded state.
+   * Experimental Feature.
+   */
+  @RestrictTo(LIBRARY_GROUP)
+  public void setMaxLines(int maxLines) {
+    collapsingTextHelper.setMaxLines(maxLines);
+  }
+
+  /**
+   * Gets the maximum number of lines to display in the expanded state.
+   * Experimental Feature.
+   */
+  @RestrictTo(LIBRARY_GROUP)
+  public int getMaxLines() {
+    return collapsingTextHelper.getMaxLines();
+  }
+
+  /**
    * Set the amount of visible height in pixels used to define when to trigger a scrim visibility
    * change.
    *
@@ -1055,7 +1069,8 @@ public class CollapsingToolbarLayout extends FrameLayout {
    * visible, otherwise they are hidden.
    *
    * @param height value in pixels used to define when to trigger a scrim visibility change
-   * @attr ref com.google.android.material.R.styleable#CollapsingToolbarLayout_scrimVisibleHeightTrigger
+   * @attr ref
+   *     com.google.android.material.R.styleable#CollapsingToolbarLayout_scrimVisibleHeightTrigger
    */
   public void setScrimVisibleHeightTrigger(@IntRange(from = 0) final int height) {
     if (scrimVisibleHeightTrigger != height) {

lib/java/com/google/android/material/internal/StaticLayoutBuilderCompat.java
@@ -185,7 +185,11 @@ final class StaticLayoutBuilderCompat {
 
 
     int availableWidth = Math.max(0, width);
-    CharSequence textToDraw = TextUtils.ellipsize(source, paint, availableWidth, ellipsize);
+    CharSequence textToDraw = source;
+    if (maxLines == 1) {
+      textToDraw = TextUtils.ellipsize(source, paint, availableWidth, ellipsize);
+    }
+
     end = Math.min(textToDraw.length(), end);
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
       if (isRtl) {

commit 6041d791702d16e7501289f939fa01874b16b32c

Use correct layout to calculate expanded bounds
Fix an issue introduced in 83e4e47d2225cd281bef195d1baf035117cda4cd
which affected multiline collapsing toolbars
when the expanded size was much larger or smaller
than the collapsed size.
The stored textLayoutHeight is only used to
calculate the expanded bounds but is generated
after the call to calculateUsingTextSize(collapsedTextSize)
which updates textLayout to the collapsed size.
This change just moves it to after
calculateUsingTextSize(expandedTextSize) so
textLayout is using the expanded size.

Note this isn't very visible in the catalog app
because the text sizes are similar but you'll
notice after applying this that the bottom
margin now applies correctly. It's more noticable
if you set the expanded text size to one of the
larger sizes.

lib/java/com/google/android/material/internal/CollapsingTextHelper.java
@@ -546,8 +546,6 @@ public final class CollapsingTextHelper {
             collapsedTextGravity,
             isRtl ? ViewCompat.LAYOUT_DIRECTION_RTL : ViewCompat.LAYOUT_DIRECTION_LTR);
 
-    float textLayoutHeight = textLayout != null ? textLayout.getHeight() : 0;
-
     switch (collapsedAbsGravity & Gravity.VERTICAL_GRAVITY_MASK) {
       case Gravity.BOTTOM:
         collapsedDrawY = collapsedBounds.bottom;
@@ -579,6 +577,8 @@ public final class CollapsingTextHelper {
     }
 
     calculateUsingTextSize(expandedTextSize);
+
+    float textLayoutHeight = textLayout != null ? textLayout.getHeight() : 0;
 
     float measuredWidth = textToDraw != null
         ? textPaint.measureText(textToDraw, 0, textToDraw.length()) : 0;