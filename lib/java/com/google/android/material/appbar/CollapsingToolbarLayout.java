7e3f5f99fa650b0282a5338eac254ec71593e795
extmkv <github@jcosta.pt>
2020-08-06 13:53:34 +0200

    If we have a scrimAnimator already created we must update the duration

--- lib/java/com/google/android/material/appbar/CollapsingToolbarLayout.java
+++ lib/java/com/google/android/material/appbar/CollapsingToolbarLayout.java
@@ -624,7 +624,6 @@ public class CollapsingToolbarLayout extends FrameLayout {
     ensureToolbar();
     if (scrimAnimator == null) {
       scrimAnimator = new ValueAnimator();
-      scrimAnimator.setDuration(scrimAnimationDuration);
       scrimAnimator.setInterpolator(
           targetAlpha > scrimAlpha
               ? AnimationUtils.FAST_OUT_LINEAR_IN_INTERPOLATOR
@@ -640,6 +639,7 @@ public class CollapsingToolbarLayout extends FrameLayout {
       scrimAnimator.cancel();
     }
 
+    scrimAnimator.setDuration(scrimAnimationDuration);
     scrimAnimator.setIntValues(scrimAlpha, targetAlpha);
     scrimAnimator.start();
   }
