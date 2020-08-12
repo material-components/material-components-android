fcea6e8ab2f6f6420b5082e06375ba5e72b2f332
Gabriele Mariotti <gabri.mariotti@gmail.com>
2020-08-12 12:39:23 -0400

    [TextInputLayout] Added method to set hint with a String resource
    Resolves https://github.com/material-components/material-components-android/issues/959
    Resolves https://github.com/material-components/material-components-android/pull/1598
    
    GIT_ORIGIN_REV_ID=c156ad372ca4055f70a7021db89d91b915db8b4c
    PiperOrigin-RevId: 326249688

--- lib/java/com/google/android/material/textfield/TextInputLayout.java
+++ lib/java/com/google/android/material/textfield/TextInputLayout.java
@@ -1463,6 +1463,17 @@ public class TextInputLayout extends LinearLayout {
     }
   }
 
+  /**
+   * Set the hint to be displayed in the floating label, if enabled, using the given resource id.
+   *
+   * @see #setHintEnabled(boolean)
+   * @see #setHint(CharSequence)
+   * @param textHintId The resource id of the text to display in the floating label
+   */
+  public void setHint(@StringRes int textHintId) {
+    setHint(textHintId != 0 ? getResources().getText(textHintId) : null);
+  }
+
   private void setHintInternal(CharSequence hint) {
     if (!TextUtils.equals(hint, this.hint)) {
       this.hint = hint;
