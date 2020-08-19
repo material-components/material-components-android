3600dccb10dd7b32a1df92e53e281a761f76ec11
Gabriele Mariotti <gabri.mariotti@gmail.com>
2020-08-20 01:08:51 +0200

    Update catalog with prefix/suffix always visible

--- catalog/java/io/material/catalog/textfield/TextFieldPrefixSuffixDemoFragment.java
+++ catalog/java/io/material/catalog/textfield/TextFieldPrefixSuffixDemoFragment.java
@@ -16,9 +16,13 @@
 
 package io.material.catalog.textfield;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import io.material.catalog.R;
-
 import androidx.annotation.LayoutRes;
+import com.google.android.material.textfield.TextInputLayout;
 
 /** A fragment that displays the filled text field demos with controls for the Catalog app. */
 public class TextFieldPrefixSuffixDemoFragment extends TextFieldControllableDemoFragment {
@@ -28,4 +32,36 @@ public class TextFieldPrefixSuffixDemoFragment extends TextFieldControllableDemo
   public int getTextFieldContent() {
     return R.layout.cat_textfield_prefix_suffix_content;
   }
+
+  @Override
+  public void initTextFieldDemoControls(LayoutInflater layoutInflater, View view) {
+    super.initTextFieldDemoControls(layoutInflater, view);
+
+    Button togglePrefixSuffixButton = view.findViewById(R.id.button_toggle_prefixsuffix);
+    togglePrefixSuffixButton.setOnClickListener(
+        v -> {
+          if (!textfields.get(0).isPrefixAlwaysVisible()) {
+            setAllPrefixSuffixAlwaysVisible(true);
+            togglePrefixSuffixButton.setText(
+                getResources().getString(R.string.cat_textfield_always_visible_false_text));
+          } else {
+            setAllPrefixSuffixAlwaysVisible(false);
+            togglePrefixSuffixButton.setText(
+                getResources().getString(R.string.cat_textfield_always_visible_true_text));
+          }
+        });
+  }
+
+  private void setAllPrefixSuffixAlwaysVisible(boolean alwaysVisible) {
+    ViewGroup parent = (ViewGroup) textfields.get(0).getParent();
+    for (TextInputLayout textfield : textfields) {
+      if (textfield.getPrefixText() != null) {
+        textfield.setPrefixAlwaysVisible(alwaysVisible);
+      }
+      if (textfield.getSuffixText() != null) {
+        textfield.setSuffixAlwaysVisible(alwaysVisible);
+      }
+    }
+  }
+
 }
