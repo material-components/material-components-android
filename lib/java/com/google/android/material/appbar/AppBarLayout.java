687e73590de5ea219a14a336094002b9d3884af5
conradchen <conradchen@google.com>
2022-06-10 07:52:24 -0700

    [TopAppBar] Exclude gone views from scroll range calculation
    Resolves https://github.com/material-components/material-components-android/issues/2709
    
    PiperOrigin-RevId: 454163860

--- lib/java/com/google/android/material/appbar/AppBarLayout.java
+++ lib/java/com/google/android/material/appbar/AppBarLayout.java
@@ -677,6 +677,10 @@ public class AppBarLayout extends LinearLayout implements CoordinatorLayout.Atta
     int range = 0;
     for (int i = 0, z = getChildCount(); i < z; i++) {
       final View child = getChildAt(i);
+      if (child.getVisibility() == GONE) {
+        // Gone views should not be included in the scroll range calculation.
+        continue;
+      }
       final LayoutParams lp = (LayoutParams) child.getLayoutParams();
       final int childHeight = child.getMeasuredHeight();
       final int flags = lp.scrollFlags;
