2465192173eabaebc8a8f5dcd1b0f5ca9330c1cb
pubiqq <serj.ese@gmail.com>
2022-12-13 11:28:10 -0500

    [TabLayout] Fix confusing documentation
    Resolves https://github.com/material-components/material-components-android/pull/3098
    
    GIT_ORIGIN_REV_ID=200a9d2f97bffa86a2b8ee3c3746fc7b5718b7a5
    PiperOrigin-RevId: 495029269

--- lib/java/com/google/android/material/tabs/TabLayoutMediator.java
+++ lib/java/com/google/android/material/tabs/TabLayoutMediator.java
@@ -220,16 +220,17 @@ public final class TabLayoutMediator {
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
       TabLayout tabLayout = tabLayoutRef.get();
       if (tabLayout != null) {
-        // Only update the text selection if we're not settling, or we are settling after
+        // Only update the tab view selection if we're not settling, or we are settling after
         // being dragged
-        boolean updateText =
+        boolean updateSelectedTabView =
             scrollState != SCROLL_STATE_SETTLING || previousScrollState == SCROLL_STATE_DRAGGING;
         // Update the indicator if we're not settling after being idle. This is caused
         // from a setCurrentItem() call and will be handled by an animation from
         // onPageSelected() instead.
         boolean updateIndicator =
             !(scrollState == SCROLL_STATE_SETTLING && previousScrollState == SCROLL_STATE_IDLE);
-        tabLayout.setScrollPosition(position, positionOffset, updateText, updateIndicator);
+        tabLayout.setScrollPosition(
+            position, positionOffset, updateSelectedTabView, updateIndicator);
       }
     }
 
