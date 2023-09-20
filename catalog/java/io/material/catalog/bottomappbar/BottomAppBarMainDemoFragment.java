39e07227cdb20fcb1c31850c4d4aff40e7b65441
manabu-nakamura <ma.nakamura@gmail.com>
2023-09-20 09:52:46 +0900

    [Catalog] When bottom sheet is collapsed, back callback is enabled

--- catalog/java/io/material/catalog/bottomappbar/BottomAppBarMainDemoFragment.java
+++ catalog/java/io/material/catalog/bottomappbar/BottomAppBarMainDemoFragment.java
@@ -236,9 +236,9 @@ public class BottomAppBarMainDemoFragment extends DemoFragment {
     switch (state) {
       case BottomSheetBehavior.STATE_EXPANDED:
       case BottomSheetBehavior.STATE_HALF_EXPANDED:
+      case BottomSheetBehavior.STATE_COLLAPSED:
         bottomDrawerOnBackPressedCallback.setEnabled(true);
         break;
-      case BottomSheetBehavior.STATE_COLLAPSED:
       case BottomSheetBehavior.STATE_HIDDEN:
         bottomDrawerOnBackPressedCallback.setEnabled(false);
         break;
