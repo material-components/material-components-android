3f205cb57f2ba41d9bef603f393f9c914254d064
Gaëtan Muller <gaetan.muller@rts.ch>
2023-11-14 10:05:26 +0100

    Cleanup outdated `@TargetApi` annotations

--- lib/javatests/com/google/android/material/datepicker/CalendarStyleTest.java
+++ lib/javatests/com/google/android/material/datepicker/CalendarStyleTest.java
@@ -19,13 +19,11 @@ import com.google.android.material.test.R;
 
 import static org.junit.Assert.assertEquals;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.InsetDrawable;
 import android.graphics.drawable.LayerDrawable;
-import android.os.Build.VERSION_CODES;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 import androidx.test.core.app.ApplicationProvider;
@@ -74,7 +72,6 @@ public class CalendarStyleTest {
   }
 
   @Test
-  @TargetApi(VERSION_CODES.KITKAT)
   public void testShapeStyling() {
     calendarStyle.day.styleItem(textView);
     Drawable backgroundDrawable;
