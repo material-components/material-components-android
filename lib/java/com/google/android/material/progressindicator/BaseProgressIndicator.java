93b30109b0ccfd1c268a074f4673eb29536467ed
pekingme <8545955+pekingme@users.noreply.github.com>
2023-12-18 14:21:05 -0800

    [ProgressIndicator] Added missing graphics updates when spec changes.
    PiperOrigin-RevId: 592008309

--- lib/java/com/google/android/material/progressindicator/BaseProgressIndicator.java
+++ lib/java/com/google/android/material/progressindicator/BaseProgressIndicator.java
@@ -634,6 +634,8 @@ public abstract class BaseProgressIndicator<S extends BaseProgressIndicatorSpec>
   public void setTrackCornerRadius(@Px int trackCornerRadius) {
     if (spec.trackCornerRadius != trackCornerRadius) {
       spec.trackCornerRadius = min(trackCornerRadius, spec.trackThickness / 2);
+      spec.validateSpec();
+      invalidate();
     }
   }
 
--- lib/java/com/google/android/material/progressindicator/CircularProgressIndicator.java
+++ lib/java/com/google/android/material/progressindicator/CircularProgressIndicator.java
@@ -163,6 +163,7 @@ public class CircularProgressIndicator
     if (spec.indicatorSize != indicatorSize) {
       spec.indicatorSize = indicatorSize;
       spec.validateSpec();
+      requestLayout();
       invalidate();
     }
   }
