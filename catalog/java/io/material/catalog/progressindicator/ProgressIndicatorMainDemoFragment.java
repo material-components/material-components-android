ba0b332742e057cbe077ed594c3847e5ff3ba3ec
pekingme <8545955+pekingme@users.noreply.github.com>
2023-12-18 14:22:01 -0800

    [ProgressIndicator] Updated main demo in Catalog.
    PiperOrigin-RevId: 592008643

--- catalog/java/io/material/catalog/progressindicator/ProgressIndicatorMainDemoFragment.java
+++ catalog/java/io/material/catalog/progressindicator/ProgressIndicatorMainDemoFragment.java
@@ -17,17 +17,21 @@ package io.material.catalog.progressindicator;
 
 import io.material.catalog.R;
 
+import static com.google.android.material.progressindicator.CircularProgressIndicator.INDICATOR_DIRECTION_CLOCKWISE;
+import static com.google.android.material.progressindicator.CircularProgressIndicator.INDICATOR_DIRECTION_COUNTERCLOCKWISE;
+import static com.google.android.material.progressindicator.LinearProgressIndicator.INDICATOR_DIRECTION_LEFT_TO_RIGHT;
+import static com.google.android.material.progressindicator.LinearProgressIndicator.INDICATOR_DIRECTION_RIGHT_TO_LEFT;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.google.android.material.materialswitch.MaterialSwitch;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
 import com.google.android.material.progressindicator.LinearProgressIndicator;
+import com.google.android.material.slider.Slider;
 import io.material.catalog.feature.DemoFragment;
 
 /**
@@ -55,27 +59,74 @@ public class ProgressIndicatorMainDemoFragment extends DemoFragment {
   public void initialize(@NonNull View view) {
     LinearProgressIndicator linearIndicator = view.findViewById(R.id.linear_indicator);
     CircularProgressIndicator circularIndicator = view.findViewById(R.id.circular_indicator);
-    EditText progressInput = view.findViewById(R.id.progress_input);
-    Button updateButton = view.findViewById(R.id.update_button);
+    Slider progressSlider = view.findViewById(R.id.progress_slider);
     MaterialSwitch determinateSwitch = view.findViewById(R.id.determinate_mode_switch);
 
-    updateButton.setOnClickListener(
-        v -> {
-          int progress;
-          try {
-            progress = Integer.parseInt(progressInput.getEditableText().toString());
-          } catch (NumberFormatException e) {
-            progress = 0;
-            progressInput.setText("0");
+    progressSlider.addOnChangeListener(
+        (slider, value, fromUser) -> {
+          if (!linearIndicator.isIndeterminate()) {
+            linearIndicator.setProgressCompat((int) value, true);
           }
-          linearIndicator.setProgressCompat(progress, true);
-          circularIndicator.setProgressCompat(progress, true);
-          determinateSwitch.setChecked(true);
+          if (!circularIndicator.isIndeterminate()) {
+            circularIndicator.setProgressCompat((int) value, true);
+          }
         });
     determinateSwitch.setOnCheckedChangeListener(
         (v, isChecked) -> {
-          linearIndicator.setIndeterminate(!isChecked);
-          circularIndicator.setIndeterminate(!isChecked);
+          if (isChecked) {
+            float progress = progressSlider.getValue();
+            linearIndicator.setProgressCompat((int) progress, true);
+            circularIndicator.setProgressCompat((int) progress, true);
+          } else {
+            linearIndicator.setProgressCompat(0, false);
+            circularIndicator.setProgressCompat(0, false);
+            linearIndicator.setIndeterminate(true);
+            circularIndicator.setIndeterminate(true);
+          }
+        });
+
+    float pixelsInDp = view.getResources().getDisplayMetrics().density;
+
+    Slider thicknessSlider = view.findViewById(R.id.thicknessSlider);
+    thicknessSlider.addOnChangeListener(
+        (slider, value, fromUser) -> {
+          int newThickness = (int) (value * pixelsInDp);
+          if (linearIndicator.getTrackThickness() != newThickness) {
+            linearIndicator.setTrackThickness(newThickness);
+          }
+          if (circularIndicator.getTrackThickness() != newThickness) {
+            circularIndicator.setTrackThickness(newThickness);
+          }
+        });
+
+    Slider cornerSlider = view.findViewById(R.id.cornerSlider);
+    cornerSlider.addOnChangeListener(
+        (slider, value, fromUser) -> {
+          int newCornerRadius = (int) (value * pixelsInDp);
+          if (linearIndicator.getTrackCornerRadius() != newCornerRadius) {
+            linearIndicator.setTrackCornerRadius(newCornerRadius);
+          }
+          if (circularIndicator.getTrackCornerRadius() != newCornerRadius) {
+            circularIndicator.setTrackCornerRadius(newCornerRadius);
+          }
+        });
+
+    MaterialSwitch reverseSwitch = view.findViewById(R.id.reverseSwitch);
+    reverseSwitch.setOnCheckedChangeListener(
+        (buttonView, isChecked) -> {
+          linearIndicator.setIndicatorDirection(
+              isChecked ? INDICATOR_DIRECTION_RIGHT_TO_LEFT : INDICATOR_DIRECTION_LEFT_TO_RIGHT);
+          circularIndicator.setIndicatorDirection(
+              isChecked ? INDICATOR_DIRECTION_COUNTERCLOCKWISE : INDICATOR_DIRECTION_CLOCKWISE);
+        });
+
+    Slider circularSizeSlider = view.findViewById(R.id.circularSizeSlider);
+    circularSizeSlider.addOnChangeListener(
+        (slider, value, fromUser) -> {
+          int newCornerRadius = (int) (value * pixelsInDp);
+          if (circularIndicator.getIndicatorSize() != newCornerRadius) {
+            circularIndicator.setIndicatorSize(newCornerRadius);
+          }
         });
   }
 }
