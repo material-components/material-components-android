1b09e58865abfdf91610da16919e7f4f28739c09
pubiqq <serj.ese@gmail.com>
2024-01-03 21:53:09 +0300

    [TimePicker] Prevent the accumulation of listeners in rotationAnimator

--- lib/java/com/google/android/material/timepicker/ClockHandView.java
+++ lib/java/com/google/android/material/timepicker/ClockHandView.java
@@ -40,6 +40,7 @@ import android.view.View;
 import android.view.ViewConfiguration;
 import androidx.annotation.Dimension;
 import androidx.annotation.FloatRange;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.Px;
 import androidx.core.view.ViewCompat;
@@ -57,7 +58,7 @@ class ClockHandView extends View {
   private static final int DEFAULT_ANIMATION_DURATION = 200;
   private final int animationDuration;
   private final TimeInterpolator animationInterpolator;
-  private final ValueAnimator rotationAnimator = new ValueAnimator();
+  @NonNull private final ValueAnimator rotationAnimator = new ValueAnimator();
   private boolean animatingOnTouchUp;
   private float downX;
   private float downY;
@@ -134,6 +135,23 @@ class ClockHandView extends View {
     scaledTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
     ViewCompat.setImportantForAccessibility(this, ViewCompat.IMPORTANT_FOR_ACCESSIBILITY_NO);
     a.recycle();
+
+    initRotationAnimator();
+  }
+
+  private void initRotationAnimator() {
+    rotationAnimator.addUpdateListener(
+        animation -> {
+          float animatedValue = (float) animation.getAnimatedValue();
+          setHandRotationInternal(animatedValue, true);
+        });
+
+    rotationAnimator.addListener(new AnimatorListenerAdapter() {
+      @Override
+      public void onAnimationCancel(Animator animation) {
+        animation.end();
+      }
+    });
   }
 
   @Override
@@ -150,9 +168,7 @@ class ClockHandView extends View {
   }
 
   public void setHandRotation(@FloatRange(from = 0f, to = 360f) float degrees, boolean animate) {
-    if (rotationAnimator != null) {
-      rotationAnimator.cancel();
-    }
+    rotationAnimator.cancel();
 
     if (!animate) {
       setHandRotationInternal(degrees, false);
@@ -163,19 +179,6 @@ class ClockHandView extends View {
     rotationAnimator.setFloatValues(animationValues.first, animationValues.second);
     rotationAnimator.setDuration(animationDuration);
     rotationAnimator.setInterpolator(animationInterpolator);
-    rotationAnimator.addUpdateListener(
-        animation -> {
-          float animatedValue = (float) animation.getAnimatedValue();
-          setHandRotationInternal(animatedValue, true);
-        });
-
-    rotationAnimator.addListener(new AnimatorListenerAdapter() {
-      @Override
-      public void onAnimationCancel(Animator animation) {
-        animation.end();
-      }
-    });
-
     rotationAnimator.start();
   }
 
