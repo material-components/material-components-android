c014faa7851ddcb01997eaad69b86bb3a2f04828
pekingme <8545955+pekingme@users.noreply.github.com>
2024-01-24 23:01:23 +0000

    [ProgressIndicator] Updated the setter of track thickness to not update track corner radius.
    PiperOrigin-RevId: 601245058
    (cherry picked from commit 9c216128c6c0c576e88dce5a53366603d1571f1d)

--- lib/java/com/google/android/material/progressindicator/BaseProgressIndicator.java
+++ lib/java/com/google/android/material/progressindicator/BaseProgressIndicator.java
@@ -548,7 +548,6 @@ public abstract class BaseProgressIndicator<S extends BaseProgressIndicatorSpec>
   public void setTrackThickness(@Px int trackThickness) {
     if (spec.trackThickness != trackThickness) {
       spec.trackThickness = trackThickness;
-      spec.trackCornerRadius = min(spec.trackCornerRadius, spec.trackThickness / 2);
       requestLayout();
     }
   }
