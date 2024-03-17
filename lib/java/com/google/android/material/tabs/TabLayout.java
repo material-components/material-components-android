8451c3c2699f2948d80d4e374a7b5d507163f591
Veniamin Vynohradov <venya.2700@gmail.com>
2024-03-17 12:30:53 +0200

    [TabLayout] Fix TabView click listener customization

--- lib/java/com/google/android/material/tabs/TabLayout.java
+++ lib/java/com/google/android/material/tabs/TabLayout.java
@@ -2173,6 +2173,8 @@ public class TabLayout extends HorizontalScrollView {
     @NonNull public TabView view;
     private int id = NO_ID;
 
+    private OnTabClickListener onTabClickListener = new DefaultOnTabClickListener();
+
     // TODO(b/76413401): make package private constructor after the widget migration is finished
     public Tab() {
       // Private constructor
@@ -2217,6 +2219,17 @@ public class TabLayout extends HorizontalScrollView {
     /** Returns the id for this tab, {@code View.NO_ID} if not set. */
     public int getId() {
       return id;
+    }
+
+    /**
+     * Register a callback to be invoked when the tab is clicked.
+     * <p>
+     * Note: By default, tab is selected on click.
+     */
+    public Tab setOnTabClickListener(@Nullable OnTabClickListener onTabClickListener) {
+      this.onTabClickListener = onTabClickListener;
+
+      return this;
     }
 
     /**
@@ -2529,6 +2542,18 @@ public class TabLayout extends HorizontalScrollView {
       contentDesc = null;
       position = INVALID_POSITION;
       customView = null;
+      onTabClickListener = new DefaultOnTabClickListener();
+    }
+
+    private static class DefaultOnTabClickListener implements OnTabClickListener {
+      @Override
+      public void onClick(@NonNull Tab tab) {
+        tab.select();
+      }
+    }
+
+    interface OnTabClickListener {
+      void onClick(@NonNull Tab tab);
     }
   }
 
@@ -2644,7 +2669,9 @@ public class TabLayout extends HorizontalScrollView {
         if (!handled) {
           playSoundEffect(SoundEffectConstants.CLICK);
         }
-        tab.select();
+        if (tab.onTabClickListener != null) {
+          tab.onTabClickListener.onClick(tab);
+        }
         return true;
       } else {
         return handled;
