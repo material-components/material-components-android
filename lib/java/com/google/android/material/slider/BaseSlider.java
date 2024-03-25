7d63dca16aadfd5fcc6de77c0d523b2db8cf3246
pubiqq <serj.ese@gmail.com>
2024-03-26 00:58:25 +0300

    [Slider] Fix support for thumb separation options in BaseSlider

--- lib/java/com/google/android/material/slider/BaseSlider.java
+++ lib/java/com/google/android/material/slider/BaseSlider.java
@@ -403,6 +403,8 @@ abstract class BaseSlider<
   @NonNull private List<Drawable> customThumbDrawablesForValues = Collections.emptyList();
 
   private float touchPosition;
+
+  private float minSeparation = 0f;
   @SeparationUnit private int separationUnit = UNIT_PX;
 
   private final int tooltipTimeoutMillis;
@@ -730,7 +732,6 @@ abstract class BaseSlider<
   }
 
   private void validateMinSeparation() {
-    final float minSeparation = getMinSeparation();
     if (minSeparation < 0) {
       throw new IllegalStateException(
           String.format(EXCEPTION_ILLEGAL_MIN_SEPARATION, minSeparation));
@@ -3356,7 +3357,7 @@ abstract class BaseSlider<
 
   /** Thumbs cannot cross each other, clamp the value to a bound or the value next to it. */
   private float getClampedValue(int idx, float value) {
-    float minSeparation = getMinSeparation();
+    float minSeparation = this.minSeparation;
     minSeparation = separationUnit == UNIT_PX ? dimenToValue(minSeparation) : minSeparation;
     if (isRtl() || isVertical()) {
       minSeparation = -minSeparation;
@@ -3374,14 +3375,21 @@ abstract class BaseSlider<
     return ((dimen - trackSidePadding) / trackWidth) * (valueFrom - valueTo) + valueFrom;
   }
 
-  protected void setSeparationUnit(int separationUnit) {
-    this.separationUnit = separationUnit;
-    dirtyConfig = true;
-    postInvalidate();
+  protected float getMinSeparation() {
+    return minSeparation;
   }
 
-  protected float getMinSeparation() {
-    return 0;
+  @SeparationUnit
+  protected int getSeparationUnit() {
+    return separationUnit;
+  }
+
+  protected void setMinSeparation(float minSeparation, @SeparationUnit int separationUnit) {
+    this.minSeparation = minSeparation;
+    this.separationUnit = separationUnit;
+
+    dirtyConfig = true;
+    postInvalidate();
   }
 
   private float getValueOfTouchPosition() {
@@ -4065,6 +4073,8 @@ abstract class BaseSlider<
     sliderState.valueTo = valueTo;
     sliderState.values = new ArrayList<>(values);
     sliderState.stepSize = stepSize;
+    sliderState.minSeparation = minSeparation;
+    sliderState.separationUnit = separationUnit;
     sliderState.hasFocus = hasFocus();
     return sliderState;
   }
@@ -4078,6 +4088,9 @@ abstract class BaseSlider<
     valueTo = sliderState.valueTo;
     setValuesInternal(sliderState.values);
     stepSize = sliderState.stepSize;
+    minSeparation = sliderState.minSeparation;
+    separationUnit = sliderState.separationUnit;
+
     if (sliderState.hasFocus) {
       requestFocus();
     }
@@ -4089,6 +4102,8 @@ abstract class BaseSlider<
     float valueTo;
     ArrayList<Float> values;
     float stepSize;
+    float minSeparation;
+    @SeparationUnit int separationUnit;
     boolean hasFocus;
 
     public static final Creator<SliderState> CREATOR =
@@ -4118,6 +4133,8 @@ abstract class BaseSlider<
       values = new ArrayList<>();
       source.readList(values, Float.class.getClassLoader());
       stepSize = source.readFloat();
+      minSeparation = source.readFloat();
+      separationUnit = source.readInt();
       hasFocus = source.createBooleanArray()[0];
     }
 
@@ -4128,6 +4145,8 @@ abstract class BaseSlider<
       dest.writeFloat(valueTo);
       dest.writeList(values);
       dest.writeFloat(stepSize);
+      dest.writeFloat(minSeparation);
+      dest.writeInt(separationUnit);
       boolean[] booleans = new boolean[1];
       booleans[0] = hasFocus;
       dest.writeBooleanArray(booleans);
