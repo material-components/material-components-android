ca566b58aba3dc7890448a33dd79551f17946eca
manabu-nakamura <ma.nakamura@gmail.com>
2024-10-02 15:12:34 +0900

    [Slider] label is gone with slider

--- lib/java/com/google/android/material/slider/BaseSlider.java
+++ lib/java/com/google/android/material/slider/BaseSlider.java
@@ -354,8 +354,6 @@ abstract class BaseSlider<
   private final ViewTreeObserver.OnGlobalLayoutListener onGlobalLayoutListener =
       this::updateLabels;
 
-  private boolean isVisible = true;
-
   /**
    * Determines the behavior of the label which can be any of the following.
    *
@@ -2683,7 +2681,7 @@ abstract class BaseSlider<
   private boolean isSliderVisibleOnScreen() {
     final Rect contentViewBounds = new Rect();
     ViewUtils.getContentView(this).getHitRect(contentViewBounds);
-    return getLocalVisibleRect(contentViewBounds) && isVisible;
+    return getLocalVisibleRect(contentViewBounds) && isShown();
   }
 
   private void ensureLabelsRemoved() {
@@ -3153,12 +3151,6 @@ abstract class BaseSlider<
     if (sliderState.hasFocus) {
       requestFocus();
     }
-  }
-
-  @Override
-  public void onVisibilityAggregated(boolean isVisible) {
-    super.onVisibilityAggregated(isVisible);
-    this.isVisible = isVisible;
   }
 
   static class SliderState extends BaseSavedState {
