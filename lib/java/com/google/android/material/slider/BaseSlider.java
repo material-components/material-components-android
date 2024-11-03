ebf3c36d354a972ad8c344e34a2df35a0a4efecd
manabu-nakamura <ma.nakamura@gmail.com>
2024-11-04 01:03:00 +0900

    [Slider] Fix slider label not moving

--- lib/java/com/google/android/material/slider/BaseSlider.java
+++ lib/java/com/google/android/material/slider/BaseSlider.java
@@ -375,11 +375,7 @@ abstract class BaseSlider<
   private final int tooltipTimeoutMillis;
 
   @NonNull
-  private final ViewTreeObserver.OnScrollChangedListener onScrollChangedListener =
-      this::updateLabels;
-
-  @NonNull
-  private final ViewTreeObserver.OnGlobalLayoutListener onGlobalLayoutListener = this::updateLabels;
+  private final ViewTreeObserver.OnDrawListener onDrawListener = this::updateLabels;
 
   @NonNull
   private final Runnable resetActiveThumbIndex =
@@ -2173,8 +2169,7 @@ abstract class BaseSlider<
     // Update factoring in the visibility of all ancestors.
     thisAndAncestorsVisible = isShown();
 
-    getViewTreeObserver().addOnScrollChangedListener(onScrollChangedListener);
-    getViewTreeObserver().addOnGlobalLayoutListener(onGlobalLayoutListener);
+    getViewTreeObserver().addOnDrawListener(onDrawListener);
     // The label is attached on the Overlay relative to the content.
     for (TooltipDrawable label : labels) {
       attachLabelToContentView(label);
@@ -2195,8 +2190,7 @@ abstract class BaseSlider<
     for (TooltipDrawable label : labels) {
       detachLabelFromContentView(label);
     }
-    getViewTreeObserver().removeOnScrollChangedListener(onScrollChangedListener);
-    getViewTreeObserver().removeOnGlobalLayoutListener(onGlobalLayoutListener);
+    getViewTreeObserver().removeOnDrawListener(onDrawListener);
     super.onDetachedFromWindow();
   }
 
@@ -3034,8 +3028,10 @@ abstract class BaseSlider<
             public void onAnimationEnd(Animator animation) {
               super.onAnimationEnd(animation);
               ViewOverlayImpl contentViewOverlay = ViewUtils.getContentViewOverlay(BaseSlider.this);
-              for (TooltipDrawable label : labels) {
-                contentViewOverlay.remove(label);
+              if (contentViewOverlay != null) {
+                for (TooltipDrawable label : labels) {
+                  contentViewOverlay.remove(label);
+                }
               }
             }
           });
