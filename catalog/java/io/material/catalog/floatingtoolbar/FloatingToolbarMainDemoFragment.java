655dba1a48e2e76fbbb833d73c7ed963b4cc1d6b
afohrman <afohrman@google.com>
2024-11-07 22:11:47 -0800

    [FloatingToolbar] Added vertical floating toolbar configuration.
    PiperOrigin-RevId: 694362191

--- catalog/java/io/material/catalog/floatingtoolbar/FloatingToolbarMainDemoFragment.java
+++ catalog/java/io/material/catalog/floatingtoolbar/FloatingToolbarMainDemoFragment.java
@@ -26,10 +26,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.ColorInt;
+import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.floatingtoolbar.FloatingToolbarLayout;
 import io.material.catalog.feature.DemoFragment;
+import io.material.catalog.feature.DemoUtils;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /** A fragment that displays the main Floating Toolbar demo for the Catalog app. */
@@ -46,60 +51,129 @@ public class FloatingToolbarMainDemoFragment extends DemoFragment {
         layoutInflater.inflate(
             R.layout.cat_floating_toolbar_fragment, viewGroup, /* attachToRoot= */ false);
 
-    TextView bodyText = (TextView) view.findViewById(R.id.body_text);
+    TextView bodyText = view.findViewById(R.id.body_text);
 
-    // Initialize bold format button.
-    MaterialButton boldButton = view.findViewById(R.id.floating_toolbar_button_bold);
-    boldButton.addOnCheckedChangeListener(
-        (button, isChecked) -> {
-          Typeface typeface = bodyText.getTypeface();
-          if (isChecked) {
-            bodyText.setTypeface(
-                typeface, typeface.isItalic() ? Typeface.BOLD_ITALIC : Typeface.BOLD);
-          } else {
-            bodyText.setTypeface(
-                Typeface.create(
-                    bodyText.getTypeface(),
-                    typeface.isItalic() ? Typeface.ITALIC : Typeface.NORMAL));
-          }
-        });
+    // Initialize group of floating toolbars.
+    List<FloatingToolbarLayout> floatingToolbars =
+        DemoUtils.findViewsWithType(view, FloatingToolbarLayout.class);
 
-    // Initialize italics format button.
-    MaterialButton italicsButton = view.findViewById(R.id.floating_toolbar_button_italic);
-    italicsButton.addOnCheckedChangeListener(
-        (button, isChecked) -> {
-          Typeface typeface = bodyText.getTypeface();
-          if (isChecked) {
-            bodyText.setTypeface(
-                typeface, typeface.isBold() ? Typeface.BOLD_ITALIC : Typeface.ITALIC);
-          } else {
-            bodyText.setTypeface(
-                Typeface.create(
-                    bodyText.getTypeface(), typeface.isBold() ? Typeface.BOLD : Typeface.NORMAL));
-          }
-        });
+    // Initialize group of bold buttons.
+    List<MaterialButton> boldButtons =
+        initializeFormatButtons(floatingToolbars, R.id.floating_toolbar_button_bold);
+    for (MaterialButton boldButton : boldButtons) {
+      boldButton.addOnCheckedChangeListener(
+          (button, isChecked) -> {
+            Typeface typeface = bodyText.getTypeface();
+            if (isChecked) {
+              bodyText.setTypeface(
+                  typeface, typeface.isItalic() ? Typeface.BOLD_ITALIC : Typeface.BOLD);
+            } else {
+              bodyText.setTypeface(
+                  Typeface.create(
+                      bodyText.getTypeface(),
+                      typeface.isItalic() ? Typeface.ITALIC : Typeface.NORMAL));
+            }
+            propagateCheckedButtonState(boldButtons, isChecked);
+          });
+    }
 
-    // Initialize underlined format button.
-    MaterialButton underlinedButton = view.findViewById(R.id.floating_toolbar_button_underlined);
-    underlinedButton.addOnCheckedChangeListener(
-        (button, isChecked) -> {
-          int paintFlags = bodyText.getPaintFlags();
-          if (isChecked) {
-            bodyText.setPaintFlags(paintFlags | Paint.UNDERLINE_TEXT_FLAG);
-          } else {
-            bodyText.setPaintFlags(paintFlags & ~Paint.UNDERLINE_TEXT_FLAG);
-          }
-        });
+    // Initialize group of italics format buttons.
+    List<MaterialButton> italicButtons =
+        initializeFormatButtons(floatingToolbars, R.id.floating_toolbar_button_italic);
+    for (MaterialButton italicButton : italicButtons) {
+      italicButton.addOnCheckedChangeListener(
+          (button, isChecked) -> {
+            Typeface typeface = bodyText.getTypeface();
+            if (isChecked) {
+              bodyText.setTypeface(
+                  typeface, typeface.isBold() ? Typeface.BOLD_ITALIC : Typeface.ITALIC);
+            } else {
+              bodyText.setTypeface(
+                  Typeface.create(
+                      bodyText.getTypeface(), typeface.isBold() ? Typeface.BOLD : Typeface.NORMAL));
+            }
+            propagateCheckedButtonState(italicButtons, isChecked);
+          });
+    }
 
-    // Initialize color text format button.
-    MaterialButton colorTextButton = view.findViewById(R.id.floating_toolbar_button_color_text);
-    colorTextButton.setOnClickListener(v -> bodyText.setTextColor(getRandomColor()));
+    // Initialize group of underline format buttons.
+    List<MaterialButton> underlineButtons =
+        initializeFormatButtons(floatingToolbars, R.id.floating_toolbar_button_underlined);
+    for (MaterialButton underlineButton : underlineButtons) {
+      underlineButton.addOnCheckedChangeListener(
+          (button, isChecked) -> {
+            int paintFlags = bodyText.getPaintFlags();
+            if (isChecked) {
+              bodyText.setPaintFlags(paintFlags | Paint.UNDERLINE_TEXT_FLAG);
+            } else {
+              bodyText.setPaintFlags(paintFlags & ~Paint.UNDERLINE_TEXT_FLAG);
+            }
+            propagateCheckedButtonState(underlineButtons, isChecked);
+          });
+    }
 
-    // Initialize color fill format button.
-    MaterialButton colorFillButton = view.findViewById(R.id.floating_toolbar_button_color_fill);
-    colorFillButton.setOnClickListener(v -> view.setBackgroundColor(getRandomColor()));
+    // Initialize color text format buttons.
+    List<MaterialButton> colorTextButtons =
+        initializeFormatButtons(floatingToolbars, R.id.floating_toolbar_button_color_text);
+    for (MaterialButton colorTextButton : colorTextButtons) {
+      colorTextButton.setOnClickListener(v -> bodyText.setTextColor(getRandomColor()));
+    }
+
+    // Initialize color fill format buttons.
+    List<MaterialButton> colorFillButtons =
+        initializeFormatButtons(floatingToolbars, R.id.floating_toolbar_button_color_fill);
+    for (MaterialButton colorFillButton : colorFillButtons) {
+      colorFillButton.setOnClickListener(v -> view.setBackgroundColor(getRandomColor()));
+    }
+
+    // Initialize orientation configuration selection controls.
+    initializeOrientationButton(
+        view, floatingToolbars, R.id.bottom_button, R.id.floating_toolbar_bottom);
+    initializeOrientationButton(
+        view, floatingToolbars, R.id.left_button, R.id.floating_toolbar_left);
+    initializeOrientationButton(view, floatingToolbars, R.id.right_button, R.id.floating_toolbar_right);
+
+    // Select bottom configuration button to represent the toolbar that's initially visible.
+    view.findViewById(R.id.bottom_button).performClick();
 
     return view;
+  }
+
+  private void initializeOrientationButton(
+      @NonNull View view,
+      @NonNull List<FloatingToolbarLayout> floatingToolbars,
+      @IdRes int buttonId,
+      @IdRes int floatingToolbarId) {
+    MaterialButton orientationButton = view.findViewById(buttonId);
+    orientationButton.setOnClickListener(
+        v -> updateFloatingToolbarVisibilities(floatingToolbars, floatingToolbarId));
+  }
+
+  private void updateFloatingToolbarVisibilities(
+      @NonNull List<FloatingToolbarLayout> floatingToolbars, @IdRes int visibleFloatingToolbarId) {
+    for (FloatingToolbarLayout floatingToolbar : floatingToolbars) {
+      if (floatingToolbar.getId() == visibleFloatingToolbarId) {
+        floatingToolbar.setVisibility(View.VISIBLE);
+      } else {
+        floatingToolbar.setVisibility(View.GONE);
+      }
+    }
+  }
+
+  @NonNull
+  private List<MaterialButton> initializeFormatButtons(
+      @NonNull List<FloatingToolbarLayout> floatingToolbars, @IdRes int formatButtonId) {
+    List<MaterialButton> formatButtons = new ArrayList<>();
+    for (FloatingToolbarLayout floatingToolbar : floatingToolbars) {
+      formatButtons.add(floatingToolbar.findViewById(formatButtonId));
+    }
+    return formatButtons;
+  }
+
+  private void propagateCheckedButtonState(List<MaterialButton> buttons, boolean isChecked) {
+    for (MaterialButton button : buttons) {
+      button.setChecked(isChecked);
+    }
   }
 
   @ColorInt
