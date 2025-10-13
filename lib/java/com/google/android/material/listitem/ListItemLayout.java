32362922af0652bc92928e401fbb7e3b95bc37e7
rightnao <rightnao@google.com>
2025-10-13 17:58:13 +0000

    [Lists] Add ViewDragHelper
    PiperOrigin-RevId: 818740364

--- lib/java/com/google/android/material/listitem/ListItemLayout.java
+++ lib/java/com/google/android/material/listitem/ListItemLayout.java
@@ -21,9 +21,16 @@ import static com.google.android.material.theme.overlay.MaterialThemeOverlay.wra
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.customview.widget.ViewDragHelper;
+import java.lang.ref.WeakReference;
 
 /**
  * A container layout for a List item.
@@ -48,9 +55,13 @@ public class ListItemLayout extends FrameLayout {
   private static final int[] FIRST_STATE_SET = { android.R.attr.state_first };
   private static final int[] MIDDLE_STATE_SET = { android.R.attr.state_middle };
   private static final int[] LAST_STATE_SET = { android.R.attr.state_last };
-  private static final int[] SINGLE_STATE_SET = { android.R.attr.state_single };
+  private static final int[] SINGLE_STATE_SET = {android.R.attr.state_single};
 
   @Nullable private int[] positionState;
+
+  @Nullable private ViewDragHelper viewDragHelper;
+  @Nullable private GestureDetector gestureDetector;
+  private WeakReference<ListItemRevealLayout> swipeToRevealLayoutRef;
 
   public ListItemLayout(@NonNull Context context) {
     this(context, null);
@@ -99,5 +110,103 @@ public class ListItemLayout extends FrameLayout {
       positionState = MIDDLE_STATE_SET;
     }
     refreshDrawableState();
+  }
+
+  @Override
+  public void addView(View child, int index, ViewGroup.LayoutParams params) {
+    super.addView(child, index, params);
+    if (swipeToRevealLayoutRef != null
+        && swipeToRevealLayoutRef.get() != null
+        && child instanceof ListItemRevealLayout) {
+      throw new UnsupportedOperationException(
+          "Only one ListItemRevealLayout is supported in a ListItemLayout.");
+    } else if (child instanceof ListItemRevealLayout) {
+      swipeToRevealLayoutRef = new WeakReference<>((ListItemRevealLayout) child);
+    }
+  }
+
+  @Override
+  public void onViewRemoved(View child) {
+    super.onViewRemoved(child);
+    if (child instanceof ListItemRevealLayout) {
+      viewDragHelper = null;
+      gestureDetector = null;
+      swipeToRevealLayoutRef = null;
+    }
+  }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent ev) {
+    if (ensureSwipeToRevealSetupIfNeeded() && viewDragHelper != null && gestureDetector != null) {
+      // TODO - b/447218120: Check that at least one child is a ListItemRevealLayout and the other
+      //  is List content.
+      // Process the event
+      viewDragHelper.processTouchEvent(ev);
+      gestureDetector.onTouchEvent(ev);
+
+      // Mark the event as handled if we are dragging
+      if (viewDragHelper.getViewDragState() == ViewDragHelper.STATE_DRAGGING) {
+        return true;
+      }
+    }
+
+    // Otherwise, defer to the default behavior.
+    return super.onTouchEvent(ev);
+  }
+
+  @Override
+  public boolean onInterceptTouchEvent(MotionEvent ev) {
+    if (ensureSwipeToRevealSetupIfNeeded()) {
+      final int action = ev.getActionMasked();
+      if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
+        viewDragHelper.cancel();
+        return false;
+      }
+      gestureDetector.onTouchEvent(ev);
+
+      // Let ViewDragHelper decide if it should intercept.
+      return viewDragHelper.shouldInterceptTouchEvent(ev);
+    }
+
+    return super.onInterceptTouchEvent(ev);
+  }
+
+  /**
+   * Returns whether or not we should swipe to reveal, and ensures that the necessary swipe
+   * variables are initialized if true.
+   */
+  private boolean ensureSwipeToRevealSetupIfNeeded() {
+    if (swipeToRevealLayoutRef == null || swipeToRevealLayoutRef.get() == null) {
+      return false;
+    }
+    if (viewDragHelper == null || gestureDetector == null) {
+      viewDragHelper =
+          ViewDragHelper.create(
+              this,
+              new ViewDragHelper.Callback() {
+                @Override
+                public boolean tryCaptureView(@NonNull View child, int pointerId) {
+                  return false;
+                }
+              });
+
+      gestureDetector =
+          new GestureDetector(
+              getContext(),
+              new SimpleOnGestureListener() {
+                @Override
+                public boolean onScroll(
+                    MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                  if (getParent() != null) {
+                    // Disallow intercepting touch event on the parent so that it doesn't interfere
+                    // with the swipe gesture. This value gets reset to false at the end of the
+                    // gesture.
+                    getParent().requestDisallowInterceptTouchEvent(true);
+                  }
+                  return false;
+                }
+              });
+    }
+    return true;
   }
 }
